a88499f59bab1f23d130a8652a7bc7ccf805aa5f
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-11-17 16:20:07 +0700

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import scipy.io
+
+Normal_0 = scipy.io.loadmat('Normal_0.mat')
+B007_0 = scipy.io.loadmat('B007_0.mat')
+IR007_0 = scipy.io.loadmat('IR007_0.mat')
+OR007@3_0 = scipy.io.loadmat('OR007@3_0.mat')
+OR007@6_0 = scipy.io.loadmat('OR007@6_0.mat')
+OR007@12_0 = scipy.io.loadmat('OR007@12_0.mat')
