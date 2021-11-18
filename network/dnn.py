f8baeacaf99baff6249b6286abdf892e3f653aee
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-11-18 22:15:20 +0700

    Update dnn.py

--- network/dnn.py
+++ network/dnn.py
@@ -1,15 +1,16 @@
-from tensorflow import keras
+import keras
+import tensorflow as tf
 
 def network():
   # Build neural network
-  input_ = keras.layers.Input(shape=[12,])
-  hidden1 = keras.layers.Dense(100, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="he_normal")(input_) # "lecun_normal"
-  hidden1 = keras.layers.AlphaDropout(rate=0.2)(hidden1),
-  # norm1 = keras.layers.BatchNormalization()(hidden1)
-  hidden2 = keras.layers.Dense(100, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="he_normal")(hidden1)
-  hidden2 = keras.layers.AlphaDropout(rate=0.2)(hidden2),
-  # norm2 = keras.layers.BatchNormalization()(hidden2)
-  concat = keras.layers.concatenate([input_, hidden2])
+  input_ = keras.layers.Input(shape=[1246, ])
+  hidden1 = keras.layers.Dense(300, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="he_normal")(input_) # "lecun_normal"
+  # hidden1 = keras.layers.AlphaDropout(rate=0.2)(hidden1),
+  norm1 = keras.layers.BatchNormalization()(hidden1)
+  hidden2 = keras.layers.Dense(100, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="he_normal")(norm1)
+  # hidden2 = keras.layers.AlphaDropout(rate=0.2)(hidden2),
+  norm2 = keras.layers.BatchNormalization()(hidden2)
+  concat = keras.layers.concatenate([input_, norm2])
   output = keras.layers.Dense(6, activation=tf.keras.layers.ReLU())(concat)
   model = keras.models.Model(inputs=[input_], outputs=[output])
   return model
