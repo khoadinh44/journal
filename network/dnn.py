22d4bca822a3527f8f6c1c8f142fe40b16906cfc
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-11-18 20:48:31 +0700

    Update dnn.py

--- network/dnn.py
+++ network/dnn.py
@@ -2,7 +2,7 @@ from tensorflow import keras
 
 def network():
   # Build neural network
-  input_ = keras.layers.Input(shape=[5,])
+  input_ = keras.layers.Input(shape=[12,])
   hidden1 = keras.layers.Dense(100, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="he_normal")(input_) # "lecun_normal"
   hidden1 = keras.layers.AlphaDropout(rate=0.2)(hidden1),
   # norm1 = keras.layers.BatchNormalization()(hidden1)
@@ -10,6 +10,6 @@ def network():
   hidden2 = keras.layers.AlphaDropout(rate=0.2)(hidden2),
   # norm2 = keras.layers.BatchNormalization()(hidden2)
   concat = keras.layers.concatenate([input_, hidden2])
-  output = keras.layers.Dense(5, activation=tf.keras.layers.ReLU())(concat)
+  output = keras.layers.Dense(6, activation=tf.keras.layers.ReLU())(concat)
   model = keras.models.Model(inputs=[input_], outputs=[output])
   return model
