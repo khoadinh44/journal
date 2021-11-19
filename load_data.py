73fa62192aece70a1c9d36ab4ca3c28d7b8752c5
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-11-19 21:38:42 +0700

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -29,8 +29,8 @@ all_labels = {0: 'Normal_0', 1: 'B007_0', 2: 'IR007_0', 3: 'OR007_3_0', 4: 'OR00
 
 Normal_0_X097_DE_time = Normal_0['X097_DE_time'][:num]
 Normal_0_X097_FE_time = Normal_0['X097_FE_time'][:num]
-# Normal_0_X097_DE_time = Fourier(f=Normal_0_X097_DE_time, num=num, get_result=True, thres=25)
-# Normal_0_X097_FE_time = Fourier(f=Normal_0_X097_FE_time, num=num, get_PSD=True, thres=25)
+Normal_0_X097_DE_time = Fourier(f=Normal_0_X097_DE_time, num=num, get_result=True, thres=25)
+Normal_0_X097_FE_time = Fourier(f=Normal_0_X097_FE_time, num=num, get_result=True, thres=25)
 Normal_0_X097RPM      = Normal_0['X097RPM']
 Normal_0_group = np.concatenate((Normal_0_X097_DE_time.reshape(1, num), Normal_0_X097_FE_time.reshape(1, num)), axis=0)
 Normal_0_name  = [[1, 0, 0, 0, 0, 0]]*n
@@ -39,8 +39,8 @@ Normal_0_reshape = Normal_0_group.reshape(n, int(num/n)*2)
 
 B007_0_X122_DE_time = B007_0['X122_DE_time'][:num]
 B007_0_X122_FE_time = B007_0['X122_FE_time'][:num]
-# B007_0_X122_DE_time = Fourier(f=B007_0_X122_DE_time, num=num, get_result=True, thres=5)
-# B007_0_X122_FE_time = Fourier(f=B007_0_X122_FE_time, num=num, get_PSD=True, thres=5)
+B007_0_X122_DE_time = Fourier(f=B007_0_X122_DE_time, num=num, get_result=True, thres=25)
+B007_0_X122_FE_time = Fourier(f=B007_0_X122_FE_time, num=num, get_result=True, thres=25)
 B007_0_X122RPM      = B007_0['X122RPM']
 B007_0_group = np.concatenate((B007_0_X122_DE_time.reshape(1, num), B007_0_X122_FE_time.reshape(1, num)), axis=0)
 B007_0_name  = [[0, 1, 0, 0, 0, 0]]*n
@@ -49,8 +49,8 @@ B007_0_reshape = B007_0_group.reshape(n, int(num/n)*2)
 
 IR007_0_X122_DE_time = IR007_0['X109_DE_time'][:num]
 IR007_0_X122_FE_time = IR007_0['X109_FE_time'][:num]
-# IR007_0_X122_DE_time = Fourier(f=IR007_0_X122_DE_time, num=num, get_result=True, thres=5)
-# IR007_0_X122_FE_time = Fourier(f=IR007_0_X122_FE_time, num=num, get_PSD=True, thres=5)
+IR007_0_X122_DE_time = Fourier(f=IR007_0_X122_DE_time, num=num, get_result=True, thres=25)
+IR007_0_X122_FE_time = Fourier(f=IR007_0_X122_FE_time, num=num, get_result=True, thres=25)
 IR007_0_X122RPM      = IR007_0['X109RPM']
 IR007_0_group = np.concatenate((IR007_0_X122_DE_time.reshape(1, num), IR007_0_X122_FE_time.reshape(1, num)), axis=0)
 IR007_0_name  = [[0, 0, 1, 0, 0, 0]]*n
@@ -59,8 +59,8 @@ IR007_0_reshape = IR007_0_group.reshape(n, int(num/n)*2)
 
 OR007_3_0_X122_DE_time = OR007_3_0['X148_DE_time'][:num]
 OR007_3_0_X122_FE_time = OR007_3_0['X148_FE_time'][:num]
-# OR007_3_0_X122_DE_time = Fourier(f=OR007_3_0_X122_DE_time, num=num, get_result=True, thres=5)
-# OR007_3_0_X122_FE_time = Fourier(f=OR007_3_0_X122_FE_time, num=num, get_PSD=True, thres=5)
+OR007_3_0_X122_DE_time = Fourier(f=OR007_3_0_X122_DE_time, num=num, get_result=True, thres=25)
+OR007_3_0_X122_FE_time = Fourier(f=OR007_3_0_X122_FE_time, num=num, get_result=True, thres=25)
 OR007_3_0_X122RPM      = OR007_3_0['X148RPM']
 OR007_3_0_group = np.concatenate((OR007_3_0_X122_DE_time.reshape(1, num), OR007_3_0_X122_FE_time.reshape(1, num)), axis=0)
 OR007_3_0_name  = [[0, 0, 0, 1, 0, 0]]*n
@@ -69,8 +69,8 @@ OR007_3_0_reshape = OR007_3_0_group.reshape(n, int(num/n)*2)
 
 OR007_6_0_X122_DE_time = OR007_6_0['X135_DE_time'][:num]
 OR007_6_0_X122_FE_time = OR007_6_0['X135_FE_time'][:num]
-# OR007_6_0_X122_DE_time = Fourier(f=OR007_6_0_X122_DE_time, num=num, get_result=True, thres=5)
-# OR007_6_0_X122_FE_time = Fourier(f=OR007_6_0_X122_FE_time, num=num, get_PSD=True, thres=5)
+OR007_6_0_X122_DE_time = Fourier(f=OR007_6_0_X122_DE_time, num=num, get_result=True, thres=25)
+OR007_6_0_X122_FE_time = Fourier(f=OR007_6_0_X122_FE_time, num=num, get_result=True, thres=25)
 OR007_6_0_X122RPM      = OR007_6_0['X135RPM']
 OR007_6_0_group = np.concatenate((OR007_6_0_X122_DE_time.reshape(1, num), OR007_6_0_X122_FE_time.reshape(1, num)), axis=0)
 OR007_6_0_name  = [[0, 0, 0, 0, 1, 0]]*n
@@ -79,8 +79,8 @@ OR007_6_0_reshape = OR007_6_0_group.reshape(n, int(num/n)*2)
 
 OR007_12_0_X122_DE_time = OR007_12_0['X161_DE_time'][:num]
 OR007_12_0_X122_FE_time = OR007_12_0['X161_FE_time'][:num]
-# OR007_12_0_X122_DE_time = Fourier(f=OR007_12_0_X122_DE_time, num=num, get_result=True, thres=5)
-# OR007_12_0_X122_FE_time = Fourier(f=OR007_12_0_X122_FE_time, num=num, get_PSD=True, thres=5)
+OR007_12_0_X122_DE_time = Fourier(f=OR007_12_0_X122_DE_time, num=num, get_result=True, thres=25)
+OR007_12_0_X122_FE_time = Fourier(f=OR007_12_0_X122_FE_time, num=num, get_result=True, thres=25)
 OR007_12_0_X122RPM      = OR007_12_0['X161RPM']
 OR007_12_0_group = np.concatenate((OR007_12_0_X122_DE_time.reshape(1, num), OR007_12_0_X122_FE_time.reshape(1, num)), axis=0)
 OR007_12_0_name  = [[0, 0, 0, 0, 0, 1]]*n
