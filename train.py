c10340ff66c621155210311c4477693a0d0498fb
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-11-19 20:00:58 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,18 +1,20 @@
 import tensorflow as tf
 import numpy as np
-from network.dnn import network
+from network.dnn import network, merge_network
 from sklearn.model_selection import train_test_split
-from load_data import Normal_0_reshape, Normal_0_name,\
-                      B007_0_reshape, B007_0_name,\
-                      IR007_0_reshape, IR007_0_name,\
-                      OR007_3_0_reshape, OR007_3_0_name,\
-                      OR007_6_0_reshape, OR007_6_0_name,\
-                      OR007_12_0_reshape, OR007_12_0_name
+from load_data import Normal_0_reshape,\
+                      B007_0_reshape,\
+                      IR007_0_reshape,\
+                      OR007_3_0_reshape,\
+                      OR007_6_0_reshape,\
+                      OR007_12_0_reshape,\
+                      merge_data, label
 
-data = np.concatenate((Normal_0_reshape, B007_0_reshape, IR007_0_reshape, OR007_3_0_reshape, OR007_6_0_reshape, OR007_12_0_reshape))
-label = np.concatenate((Normal_0_name, B007_0_name, IR007_0_name, OR007_3_0_name, OR007_6_0_name, OR007_12_0_name))
+# data = np.concatenate((Normal_0_reshape, B007_0_reshape, IR007_0_reshape, OR007_3_0_reshape, OR007_6_0_reshape, OR007_12_0_reshape))
 
-X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.1, random_state=42, shuffle=True)
+X_train, X_test, y_train, y_test = train_test_split(merge_data, label, test_size=0.1, random_state=42, shuffle=True)
+X_train_A, X_train_B = X_train[:, :623], X_train[:, 623:]
+X_test_A, X_test_B = X_test[:, :623], X_test[:, 623:]
 # active GPU
 # tf.debugging.set_log_device_placement(True)
 
@@ -27,4 +29,4 @@ def train(data=None, labels=None,\
                     validation_data=(val_data, val_labels))
   model.save(name_saver)
 
-train(X_train, y_train, X_test, y_test, network, 200, 32, True, 'model.h5')
+train((X_train_A, X_train_B), y_train, (X_test_A, X_test_B), y_test, merge_network, 200, 32, True, 'model.h5')
