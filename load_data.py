eb08392e7774e8f3b94cc0e1bd3c9432267065f1
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-11-20 10:58:21 +0700

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -4,8 +4,11 @@ import scipy.io
 from preprocessing.denoise_signal import Fourier, SVD_denoise, Wavelet
 import matplotlib.pyplot as plt
 
-use_Fourier=False
 use_network=False
+
+# actived merge_network()
+use_Fourier=True
+use_Wavelet=False
 '''
 For all files, the following item in the variable name indicates:
     DE - drive end accelerometer data
@@ -21,12 +24,13 @@ For all files, the following item in the variable name indicates:
 num = 124600
 n   = 200
 
-OR007_12_0_name  = [[0, 0, 0, 0, 0, 1]]*n
-B007_0_name      = [[0, 1, 0, 0, 0, 0]]*n
-IR007_0_name     = [[0, 0, 1, 0, 0, 0]]*n
-OR007_3_0_name   = [[0, 0, 0, 1, 0, 0]]*n
-OR007_6_0_name   = [[0, 0, 0, 0, 1, 0]]*n
-Normal_0_name    = [[1, 0, 0, 0, 0, 0]]*n
+Normal_0_name    = [[1, 0, 0, 0, 0, 0]]*int(num/n)
+B007_0_name      = [[0, 1, 0, 0, 0, 0]]*int(num/n)
+IR007_0_name     = [[0, 0, 1, 0, 0, 0]]*int(num/n)
+OR007_3_0_name   = [[0, 0, 0, 1, 0, 0]]*int(num/n)
+OR007_6_0_name   = [[0, 0, 0, 0, 1, 0]]*int(num/n)
+OR007_12_0_name  = [[0, 0, 0, 0, 0, 1]]*int(num/n)
+
 label = np.concatenate((Normal_0_name, B007_0_name, IR007_0_name, OR007_3_0_name, OR007_6_0_name, OR007_12_0_name))
 
 Normal_0 = scipy.io.loadmat('./data/Normal_0.mat')
@@ -91,6 +95,22 @@ if use_Wavelet:
   OR007_12_0_X122_DE_time_0, OR007_12_0_X122_DE_time_1, OR007_12_0_X122_DE_time_2 = SVD_denoise(OR007_12_0_X122_DE_time)
   OR007_12_0_X122_DE_time_0, OR007_12_0_X122_DE_time_1, OR007_12_0_X122_DE_time_2 = SVD_denoise(OR007_12_0_X122_DE_time)
 
+  DE_time_1 = np.concatenate((Normal_0_X097_DE_time_1.reshape(int(num/n), n), B007_0_X122_DE_time_1.reshape(int(num/n), n), IR007_0_X122_DE_time_1.reshape(int(num/n), n), OR007_3_0_X122_DE_time_1.reshape(int(num/n), n), OR007_6_0_X122_DE_time_1.reshape(int(num/n), n), OR007_12_0_X122_DE_time_1.reshape(int(num/n), n)))
+  FE_time_1 = np.concatenate((Normal_0_X097_FE_time_1.reshape(int(num/n), n), B007_0_X122_FE_time_1.reshape(int(num/n), n), IR007_0_X122_FE_time_1.reshape(int(num/n), n), OR007_3_0_X122_FE_time_1.reshape(int(num/n), n), OR007_6_0_X122_FE_time_1.reshape(int(num/n), n), OR007_12_0_X122_FE_time_1.reshape(int(num/n), n)))
+  merge_data_1 = np.concatenate((DE_time_1, FE_time_1), axis=1)
+
+  DE_time_0 = np.concatenate((Normal_0_X097_DE_time_0.reshape(int(num/n), n), B007_0_X122_DE_time_0.reshape(int(num/n), n), IR007_0_X122_DE_time_0.reshape(int(num/n), n), OR007_3_0_X122_DE_time_0.reshape(int(num/n), n), OR007_6_0_X122_DE_time_0.reshape(int(num/n), n), OR007_12_0_X122_DE_time_0.reshape(int(num/n), n)))
+  FE_time_0 = np.concatenate((Normal_0_X097_FE_time_0.reshape(int(num/n), n), B007_0_X122_FE_time_0.reshape(int(num/n), n), IR007_0_X122_FE_time_0.reshape(int(num/n), n), OR007_3_0_X122_FE_time_0.reshape(int(num/n), n), OR007_6_0_X122_FE_time_0.reshape(int(num/n), n), OR007_12_0_X122_FE_time_0.reshape(int(num/n), n)))
+  merge_data_0 = np.concatenate((DE_time_0, FE_time_0), axis=1)
+
+  DE_time_1 = np.concatenate((Normal_0_X097_DE_time_1.reshape(int(num/n), n), B007_0_X122_DE_time_1.reshape(int(num/n), n), IR007_0_X122_DE_time_1.reshape(int(num/n), n), OR007_3_0_X122_DE_time_1.reshape(int(num/n), n), OR007_6_0_X122_DE_time_1.reshape(int(num/n), n), OR007_12_0_X122_DE_time_1.reshape(int(num/n), n)))
+  FE_time_1 = np.concatenate((Normal_0_X097_FE_time_1.reshape(int(num/n), n), B007_0_X122_FE_time_1.reshape(int(num/n), n), IR007_0_X122_FE_time_1.reshape(int(num/n), n), OR007_3_0_X122_FE_time_1.reshape(int(num/n), n), OR007_6_0_X122_FE_time_1.reshape(int(num/n), n), OR007_12_0_X122_FE_time_1.reshape(int(num/n), n)))
+  merge_data_1 = np.concatenate((DE_time_1, FE_time_1), axis=1)
+
+  DE_time_2 = np.concatenate((Normal_0_X097_DE_time_2.reshape(int(num/n), n), B007_0_X122_DE_time_2.reshape(int(num/n), n), IR007_0_X122_DE_time_2.reshape(int(num/n), n), OR007_3_0_X122_DE_time_2.reshape(int(num/n), n), OR007_6_0_X122_DE_time_2.reshape(int(num/n), n), OR007_12_0_X122_DE_time_2.reshape(int(num/n), n)))
+  FE_time_2 = np.concatenate((Normal_0_X097_FE_time_2.reshape(int(num/n), n), B007_0_X122_FE_time_2.reshape(int(num/n), n), IR007_0_X122_FE_time_2.reshape(int(num/n), n), OR007_3_0_X122_FE_time_2.reshape(int(num/n), n), OR007_6_0_X122_FE_time_2.reshape(int(num/n), n), OR007_12_0_X122_FE_time_2.reshape(int(num/n), n)))
+  merge_data_2 = np.concatenate((DE_time_2, FE_time_2), axis=1)
+
 
 
 if use_Fourier:
@@ -112,7 +132,7 @@ if use_Fourier:
   OR007_12_0_X122_DE_time = Fourier(f=OR007_12_0_X122_DE_time, num=num, get_result=True, thres=25)
   OR007_12_0_X122_FE_time = Fourier(f=OR007_12_0_X122_FE_time, num=num, get_result=True, thres=85)
 
-  DE_time = np.concatenate((Normal_0_X097_DE_time.reshape(n, int(num/n)), B007_0_X122_DE_time.reshape(n, int(num/n)), IR007_0_X122_DE_time.reshape(n, int(num/n)), OR007_3_0_X122_DE_time.reshape(n, int(num/n)), OR007_6_0_X122_DE_time.reshape(n, int(num/n)), OR007_12_0_X122_DE_time.reshape(n, int(num/n))))
-  FE_time = np.concatenate((Normal_0_X097_FE_time.reshape(n, int(num/n)), B007_0_X122_FE_time.reshape(n, int(num/n)), IR007_0_X122_FE_time.reshape(n, int(num/n)), OR007_3_0_X122_FE_time.reshape(n, int(num/n)), OR007_6_0_X122_FE_time.reshape(n, int(num/n)), OR007_12_0_X122_FE_time.reshape(n, int(num/n))))
+  DE_time = np.concatenate((Normal_0_X097_DE_time.reshape(int(num/n), n), B007_0_X122_DE_time.reshape(int(num/n), n), IR007_0_X122_DE_time.reshape(int(num/n), n), OR007_3_0_X122_DE_time.reshape(int(num/n), n), OR007_6_0_X122_DE_time.reshape(int(num/n), n), OR007_12_0_X122_DE_time.reshape(int(num/n), n)))
+  FE_time = np.concatenate((Normal_0_X097_FE_time.reshape(int(num/n), n), B007_0_X122_FE_time.reshape(int(num/n), n), IR007_0_X122_FE_time.reshape(int(num/n), n), OR007_3_0_X122_FE_time.reshape(int(num/n), n), OR007_6_0_X122_FE_time.reshape(int(num/n), n), OR007_12_0_X122_FE_time.reshape(int(num/n), n)))
 
   merge_data = np.concatenate((DE_time, FE_time), axis=1)
