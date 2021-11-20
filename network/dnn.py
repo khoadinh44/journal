1839565d929d5c94d5ff6af9adbb4ce55abdc954
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-11-20 12:01:44 +0700

    Update dnn.py

--- network/dnn.py
+++ network/dnn.py
@@ -11,26 +11,33 @@ network+None | merge_network+None | merge_network+Fourier  | merge_network+SVD_d
 
 def network():
   # Build neural network
-  input_ = keras.layers.Input(shape=[1246, ])
-  hidden1 = keras.layers.Dense(500, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="lecun_normal")(input_) # "lecun_normal"
-  norm1 = keras.layers.BatchNormalization()(hidden1)
-  hidden2 = keras.layers.Dense(300, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="lecun_normal")(norm1) # "lecun_normal"
-  norm2 = keras.layers.BatchNormalization()(hidden2)
-  hidden3 = keras.layers.Dense(300, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="lecun_normal")(norm2)
-  norm3 = keras.layers.BatchNormalization()(hidden3)
-  hidden4 = keras.layers.Dense(100, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="lecun_normal")(norm3)
-  norm4 = keras.layers.BatchNormalization()(hidden4)
-  concat = keras.layers.concatenate([input_, norm4])
-  output = keras.layers.Dense(6, activation=tf.keras.layers.Softmax())(concat)
-  model = keras.models.Model(inputs=[input_], outputs=[output])
+  
   return model
 
-def merge_network():
-  input_A = keras.layers.Input(shape=[200], name="wide_input")
-  input_B = keras.layers.Input(shape=[200], name="deep_input")
-  hidden1 = keras.layers.Dense(30, activation="relu")(input_B)
-  hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
-  concat = keras.layers.concatenate([input_A, hidden2])
-  output = keras.layers.Dense(6, name="output")(concat)
-  model = keras.models.Model(inputs=[input_A, input_B], outputs=[output])
+def merge_network(use_network=False, use_Wavelet=False, use_Fourier=False):
+  if use_network:
+    input_ = keras.layers.Input(shape=[1246, ])
+    hidden1 = keras.layers.Dense(500, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="lecun_normal")(input_) # "lecun_normal"
+    norm1 = keras.layers.BatchNormalization()(hidden1)
+    hidden2 = keras.layers.Dense(300, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="lecun_normal")(norm1) # "lecun_normal"
+    norm2 = keras.layers.BatchNormalization()(hidden2)
+    hidden3 = keras.layers.Dense(300, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="lecun_normal")(norm2)
+    norm3 = keras.layers.BatchNormalization()(hidden3)
+    hidden4 = keras.layers.Dense(100, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="lecun_normal")(norm3)
+    norm4 = keras.layers.BatchNormalization()(hidden4)
+    concat = keras.layers.concatenate([input_, norm4])
+    output = keras.layers.Dense(6, activation=tf.keras.layers.Softmax())(concat)
+    model = keras.models.Model(inputs=[input_], outputs=[output])
+  else:
+    if use_Wavelet:
+      input_A = keras.layers.Input(shape=[150], name="wide_input")
+      input_B = keras.layers.Input(shape=[150], name="deep_input") 
+    if use_Fourier:
+      input_A = keras.layers.Input(shape=[200], name="wide_input")
+      input_B = keras.layers.Input(shape=[200], name="deep_input") 
+    hidden1 = keras.layers.Dense(30, activation="relu")(input_B)
+    hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+    concat = keras.layers.concatenate([input_A, hidden2])
+    output = keras.layers.Dense(6, name="output")(concat)
+    model = keras.models.Model(inputs=[input_A, input_B], outputs=[output])
   return model
