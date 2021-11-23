2373d52548e97512270d7de2001d5b1ce79868ce
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-11-23 20:38:25 +0700

    Update train.py

--- train.py
+++ train.py
@@ -2,24 +2,32 @@ import tensorflow as tf
 import numpy as np
 from network.dnn import network, merge_network
 from sklearn.model_selection import train_test_split
-from load_data import merge_data_2, label, use_network, use_Wavelet, use_Fourier
+from load_data import label, use_network, use_Wavelet, use_Fourier, use_Wavelet_denoise
 
-# active GPU
-# tf.debugging.set_log_device_placement(True) .reshape(X_train.shape[0], 300)
+if use_Wavelet:
+  from load_data import merge_data_0, merge_data_1, merge_data_2
+if use_Fourier or use_Wavelet_denoise:
+  from load_data import merge_data
 
-X_train, X_test, y_train, y_test = train_test_split(merge_data_2, label, test_size=0.1, random_state=42, shuffle=True)
-X_train_A, X_train_B             = X_train[:, :100, :], X_train[:, 100:, :]
-X_test_A, X_test_B               = X_test[:, :100, :],  X_test[:, 100:, :]
+if use_Wavelet:
+  X_train, X_test, y_train, y_test = train_test_split(merge_data_2, label, test_size=0.1, random_state=42, shuffle=True)
+  X_train_A, X_train_B             = X_train[:, :100, :], X_train[:, 100:, :]
+  X_test_A, X_test_B               = X_test[:, :100, :],  X_test[:, 100:, :]
 
-X_train_A, X_train_B             = X_train_A.reshape(int(X_train_A.shape[0]), 300), X_train_B.reshape(int(X_train_B.shape[0]), 300)   
-X_test_A, X_test_B               = X_test_A.reshape(int(X_test_A.shape[0]), 300),  X_test_B.reshape(int(X_test_B.shape[0]), 300)
+  X_train_A, X_train_B             = X_train_A.reshape(int(X_train_A.shape[0]), 300), X_train_B.reshape(int(X_train_B.shape[0]), 300)   
+  X_test_A, X_test_B               = X_test_A.reshape(int(X_test_A.shape[0]), 300),  X_test_B.reshape(int(X_test_B.shape[0]), 300)
+
+if use_Fourier or use_Wavelet_denoise:
+  X_train, X_test, y_train, y_test = train_test_split(merge_data, label, test_size=0.1, random_state=42, shuffle=True)
+  X_train_A, X_train_B             = X_train[:, :200], X_train[:, 200:]
+  X_test_A, X_test_B               = X_test[:, :200],  X_test[:, 200:]
 
 # Load model------------------------------------------------------------------------------------
 def train(data=None, labels=None,\
           val_data=None, val_labels=None,\
           network=None, num_epochs=20,\
           batch_size=32, show_metric=True, name_saver=None):
-  model = network(use_Wavelet=use_Wavelet)
+  model = network(use_Wavelet_denoise=use_Wavelet_denoise)
   model.compile(optimizer="Adam", loss="mse", metrics=["mae", "acc"])
   history = model.fit(data, labels, epochs=num_epochs,
                     validation_data=(val_data, val_labels))
