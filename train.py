b87c66e1f373519248dd37396fe85d2f48a9b4f8
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-11-26 20:47:49 +0700

    Update train.py

--- train.py
+++ train.py
@@ -2,11 +2,11 @@ import tensorflow as tf
 import numpy as np
 from network.dnn import network, merge_network
 from sklearn.model_selection import train_test_split
-from load_data import label, use_network, use_Wavelet, use_Fourier, use_Wavelet_denoise
+from load_data import label, use_network, use_Wavelet, use_Fourier, use_Wavelet_denoise, use_SVD
 
 if use_Wavelet:
   from load_data import merge_data_0, merge_data_1, merge_data_2
-if use_Fourier or use_Wavelet_denoise:
+if use_Fourier or use_Wavelet_denoise or use_SVD:
   from load_data import merge_data
 
 if use_Wavelet:
@@ -17,7 +17,7 @@ if use_Wavelet:
   X_train_A, X_train_B             = X_train_A.reshape(int(X_train_A.shape[0]), 300), X_train_B.reshape(int(X_train_B.shape[0]), 300)   
   X_test_A, X_test_B               = X_test_A.reshape(int(X_test_A.shape[0]), 300),  X_test_B.reshape(int(X_test_B.shape[0]), 300)
 
-if use_Fourier or use_Wavelet_denoise:
+if use_Fourier or use_Wavelet_denoise or use_SVD:
   X_train, X_test, y_train, y_test = train_test_split(merge_data, label, test_size=0.1, random_state=42, shuffle=True)
   X_train_A, X_train_B             = X_train[:, :200], X_train[:, 200:]
   X_test_A, X_test_B               = X_test[:, :200],  X_test[:, 200:]
@@ -27,7 +27,7 @@ def train(data=None, labels=None,\
           val_data=None, val_labels=None,\
           network=None, num_epochs=20,\
           batch_size=32, show_metric=True, name_saver=None):
-  model = network(use_Wavelet_denoise=use_Wavelet_denoise)
+  model = network(use_SVD=use_SVD)
   model.compile(optimizer="Adam", loss="mse", metrics=["mae", "acc"])
   history = model.fit(data, labels, epochs=num_epochs,
                     validation_data=(val_data, val_labels))
