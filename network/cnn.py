9bd911070d83f422dbb10c74246402b077459e77
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-12-14 14:30:55 +0700

    Update cnn.py

--- network/cnn.py
+++ network/cnn.py
@@ -4,18 +4,17 @@ import keras
 def network():
   DefaultConv2D = partial(keras.layers.Conv2D, kernel_size=3, activation='relu', padding="SAME")
   model = keras.models.Sequential([
-      DefaultConv2D(filters=64, kernel_size=7, input_shape=[28, 28, 1]),
-      keras.layers.MaxPooling2D(pool_size=2),
-      DefaultConv2D(filters=128),
-      DefaultConv2D(filters=128),
-      keras.layers.MaxPooling2D(pool_size=2),
-      DefaultConv2D(filters=256),
-      DefaultConv2D(filters=256),
-      keras.layers.MaxPooling2D(pool_size=2),
-      keras.layers.Flatten(),
-      keras.layers.Dense(units=128, activation='relu'),
-      keras.layers.Dropout(0.5),
-      keras.layers.Dense(units=64, activation='relu'),
-      keras.layers.Dropout(0.5),
-      keras.layers.Dense(units=10, activation='softmax'),])
+            DefaultConv2D(filters=256, kernel_size=7, input_shape=[128, 128, 1]), #
+            tf.keras.layers.ReLU(), #
+            keras.layers.MaxPooling2D(pool_size=3), #
+            keras.layers.Dropout(0.5), #
+            DefaultConv2D(filters=256), #
+            tf.keras.layers.ReLU(), #
+            keras.layers.MaxPooling2D(pool_size=2),#
+            keras.layers.Flatten(),
+            keras.layers.Dense(units=512, activation='relu'),
+            keras.layers.Dropout(0.5),
+            keras.layers.Dense(units=10, activation='softmax'),])
+
+  model.summary()
   return model
