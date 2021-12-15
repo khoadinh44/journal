09428fbfce7b5bac3b40edf60bef861c48f7f72b
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-12-15 21:16:27 +0700

    Update load_cnn.py

--- load_cnn.py
+++ load_cnn.py
@@ -1,56 +1,31 @@
+import numpy as np
+import pandas as pd
+import scipy.io
+import tensorflow as tf
+from preprocessing.denoise_signal import Fourier, SVD_denoise, Wavelet, Wavelet_denoise, savitzky_golay
+import matplotlib.pyplot as plt
 
-def decode_audio(audio_binary):
-  # Decode WAV-encoded audio files to `float32` tensors, normalized
-  # to the [-1.0, 1.0] range. Return `float32` audio and a sample rate.
-  audio, _ = tf.audio.decode_wav(contents=audio_binary)
-  # Since all the data is single channel (mono), drop the `channels`
-  # axis from the array.
-  return tf.squeeze(audio, axis=-1)
-
-def get_label(file_path):
-  parts = tf.strings.split(input=file_path, sep=os.path.sep)
-  # Note: You'll use indexing here instead of tuple unpacking to enable this
-  # to work in a TensorFlow graph.
-  return parts[-2]
-
-def get_waveform_and_label(file_path):
-  label = get_label(file_path)
-  audio_binary = tf.io.read_file(file_path)
-  waveform = decode_audio(audio_binary)
-  return waveform, label
-
-AUTOTUNE = tf.data.AUTOTUNE
-files_ds = tf.data.Dataset.from_tensor_slices(train_files)
-waveform_ds = files_ds.map(map_func=get_waveform_and_label,
-                           num_parallel_calls=AUTOTUNE)
-
-rows = 3
-cols = 3
-n = rows * cols
-fig, axes = plt.subplots(rows, cols, figsize=(10, 12))
-
-for i, (audio, label) in enumerate(waveform_ds.take(n)):
-  r = i // cols
-  c = i % cols
-  ax = axes[r][c]
-  ax.plot(audio.numpy())
-  ax.set_yticks(np.arange(-1.2, 1.2, 0.2))
-  label = label.numpy().decode('utf-8')
-  ax.set_title(label)
-plt.show()
+use_network         = False
+use_Fourier         = True
+use_savitzky_golay  = True
+use_Wavelet         = False
+use_Wavelet_denoise = False
+use_SVD             = False
 
 def get_spectrogram(waveform):
   # Zero-padding for an audio waveform with less than 16,000 samples.
-  input_len = 16000
+  input_len = 300
+  respect_input_len = 128*128 
   waveform = waveform[:input_len]
-  zero_padding = tf.zeros([16000] - tf.shape(waveform), dtype=tf.float32)
+  zero_padding = tf.zeros([(respect_input_len)] - tf.shape(waveform), dtype=tf.float32)
   # Cast the waveform tensors' dtype to float32.
   waveform = tf.cast(waveform, dtype=tf.float32)
   # Concatenate the waveform with `zero_padding`, which ensures all audio
   # clips are of the same length.
   equal_length = tf.concat([waveform, zero_padding], 0)
   # Convert the waveform to a spectrogram via a STFT.
-  spectrogram = tf.signal.stft(equal_length, frame_length=255, frame_step=128)
+  spectrogram = tf.signal.stft(equal_length, frame_length=255, frame_step=127, fft_length=128*2-1)
+  # spectrogram = tf.signal.stft(equal_length, frame_length=255, frame_step=128-1)
   # Obtain the magnitude of the STFT.
   spectrogram = tf.abs(spectrogram)
   # Add a `channels` dimension, so that the spectrogram can be used
@@ -59,91 +34,75 @@ def get_spectrogram(waveform):
   spectrogram = spectrogram[..., tf.newaxis]
   return spectrogram
 
-for waveform, label in waveform_ds.take(1):
-  label = label.numpy().decode('utf-8')
-  print(waveform.shape)
-  spectrogram = get_spectrogram(waveform)
+'''
+For all files, the following item in the variable name indicates:
+    DE - drive end accelerometer data
+    FE - fan end accelerometer data
+    BA - base accelerometer data
+    time - time series data
+    RPM - rpm during testing
 
-print('Label:', label)
-print('Waveform shape:', waveform.shape)
-print('Spectrogram shape:', spectrogram.shape)
-print('Audio playback')
-display.display(display.Audio(waveform, rate=16000))
+    Fault Diameter: 0.007"
+    Motor Load (HP): 0
+    Approx. Motor Speed (rpm): 1797
+'''
+num = 124600
+n   = 200
 
-def plot_spectrogram(spectrogram, ax):
-  if len(spectrogram.shape) > 2:
-    assert len(spectrogram.shape) == 3
-    spectrogram = np.squeeze(spectrogram, axis=-1)
-  # Convert the frequencies to log scale and transpose, so that the time is
-  # represented on the x-axis (columns).
-  # Add an epsilon to avoid taking a log of zero.
-  log_spec = np.log(spectrogram.T + np.finfo(float).eps)
-  height = log_spec.shape[0]
-  width = log_spec.shape[1]
-  X = np.linspace(0, np.size(spectrogram), num=width, dtype=int)
-  Y = range(height)
-  ax.pcolormesh(X, Y, log_spec)
-  
-fig, axes = plt.subplots(2, figsize=(12, 8))
-timescale = np.arange(waveform.shape[0])
-axes[0].plot(timescale, waveform.numpy())
-axes[0].set_title('Waveform')
-axes[0].set_xlim([0, 16000])
+Normal_0_name    = [[1, 0, 0, 0, 0, 0]]*int(num/n)
+B007_0_name      = [[0, 1, 0, 0, 0, 0]]*int(num/n)
+IR007_0_name     = [[0, 0, 1, 0, 0, 0]]*int(num/n)
+OR007_3_0_name   = [[0, 0, 0, 1, 0, 0]]*int(num/n)
+OR007_6_0_name   = [[0, 0, 0, 0, 1, 0]]*int(num/n)
+OR007_12_0_name  = [[0, 0, 0, 0, 0, 1]]*int(num/n)
 
-plot_spectrogram(spectrogram.numpy(), axes[1])
-axes[1].set_title('Spectrogram')
-plt.show()
+label = np.concatenate((Normal_0_name, B007_0_name, IR007_0_name, OR007_3_0_name, OR007_6_0_name, OR007_12_0_name))
 
-def get_spectrogram_and_label_id(audio, label):
-  spectrogram = get_spectrogram(audio)
-  label_id = tf.argmax(label == commands)
-  return spectrogram, label_id
+Normal_0 = scipy.io.loadmat('./data/Normal_0.mat')
+B007_0 = scipy.io.loadmat('./data/B007_0.mat')
+IR007_0 = scipy.io.loadmat('./data/IR007_0.mat')
+OR007_3_0 = scipy.io.loadmat('./data/OR007_3_0.mat')
+OR007_6_0 = scipy.io.loadmat('./data/OR007_6_0.mat')
+OR007_12_0 = scipy.io.loadmat('./data/OR007_12_0.mat')
+all_labels = {0: 'Normal_0', 1: 'B007_0', 2: 'IR007_0', 3: 'OR007_3_0', 4: 'OR007_6_0', 5: 'OR007_12_0'}
 
-spectrogram_ds = waveform_ds.map(
-  map_func=get_spectrogram_and_label_id,
-  num_parallel_calls=AUTOTUNE)
+Normal_0_X097_DE_time   = Normal_0['X097_DE_time'][:num].reshape((num//200, 200))
+Normal_0_X097_FE_time   = Normal_0['X097_FE_time'][:num].reshape((num//200, 200))
+Normal_0_X097 = np.concatenate((Normal_0_X097_DE_time, Normal_0_X097_FE_time), axis=1)
 
-rows = 3
-cols = 3
-n = rows*cols
-fig, axes = plt.subplots(rows, cols, figsize=(10, 10))
+B007_0_X122_DE_time     = B007_0['X122_DE_time'][:num].reshape((num//200, 200))
+B007_0_X122_FE_time     = B007_0['X122_FE_time'][:num].reshape((num//200, 200))
+B007_0_X122 = np.concatenate((B007_0_X122_DE_time, B007_0_X122_FE_time), axis=1)
 
-for i, (spectrogram, label_id) in enumerate(spectrogram_ds.take(n)):
-  r = i // cols
-  c = i % cols
-  ax = axes[r][c]
-  plot_spectrogram(spectrogram.numpy(), ax)
-  ax.set_title(commands[label_id.numpy()])
-  ax.axis('off')
+IR007_0_X122_DE_time    = IR007_0['X109_DE_time'][:num].reshape((num//200, 200))
+IR007_0_X122_FE_time    = IR007_0['X109_FE_time'][:num].reshape((num//200, 200))
+IR007_0_X122 = np.concatenate((IR007_0_X122_DE_time, IR007_0_X122_FE_time), axis=1)
 
-plt.show()
+OR007_3_0_X122_DE_time  = OR007_3_0['X148_DE_time'][:num].reshape((num//200, 200))
+OR007_3_0_X122_FE_time  = OR007_3_0['X148_FE_time'][:num].reshape((num//200, 200))
+OR007_3_0_X122 = np.concatenate((OR007_3_0_X122_DE_time, OR007_3_0_X122_FE_time), axis=1)
 
-def preprocess_dataset(files):
-  files_ds = tf.data.Dataset.from_tensor_slices(files)
-  output_ds = files_ds.map(map_func=get_waveform_and_label,
-                           num_parallel_calls=AUTOTUNE)
-  output_ds = output_ds.map(map_func=get_spectrogram_and_label_id,
-                            num_parallel_calls=AUTOTUNE)
-  return output_ds
+OR007_6_0_X122_DE_time  = OR007_6_0['X135_DE_time'][:num].reshape((num//200, 200))
+OR007_6_0_X122_FE_time  = OR007_6_0['X135_FE_time'][:num].reshape((num//200, 200))
+OR007_6_0_X122 = np.concatenate((OR007_6_0_X122_DE_time, OR007_6_0_X122_FE_time), axis=1)
 
-train_ds = spectrogram_ds
-val_ds = preprocess_dataset(val_files)
-test_ds = preprocess_dataset(test_files)
+OR007_12_0_X122_DE_time = OR007_12_0['X161_DE_time'][:num].reshape((num//200, 200))
+OR007_12_0_X122_FE_time = OR007_12_0['X161_FE_time'][:num].reshape((num//200, 200))
+OR007_12_0_X122 = np.concatenate((OR007_12_0_X122_DE_time, OR007_12_0_X122_FE_time), axis=1)
 
-batch_size = 64
-train_ds = train_ds.batch(batch_size)
-val_ds = val_ds.batch(batch_size)
+Normal_0_X097RPM       = Normal_0['X097RPM']
+B007_0_X122RPM         = B007_0['X122RPM']
+IR007_0_X122RPM        = IR007_0['X109RPM']
+OR007_3_0_X122RPM      = OR007_3_0['X148RPM']
+OR007_6_0_X122RPM      = OR007_6_0['X135RPM']
+OR007_12_0_X122RPM     = OR007_12_0['X161RPM']
 
-train_ds = train_ds.cache().prefetch(AUTOTUNE)
-val_ds = val_ds.cache().prefetch(AUTOTUNE)
+Normal_0_X097 = np.array([get_spectrogram(i) for i in Normal_0_X097])
+B007_0_X122 = np.array([get_spectrogram(i) for i in B007_0_X122])
+IR007_0_X122 = np.array([get_spectrogram(i) for i in IR007_0_X122])
+OR007_3_0_X122 = np.array([get_spectrogram(i) for i in OR007_3_0_X122])
+OR007_6_0_X122 = np.array([get_spectrogram(i) for i in OR007_6_0_X122])
+OR007_12_0_X122 = np.array([get_spectrogram(i) for i in OR007_12_0_X122])
 
-for spectrogram, _ in spectrogram_ds.take(1):
-  input_shape = spectrogram.shape
-print('Input shape:', input_shape)
-num_labels = len(commands)
-
-# Instantiate the `tf.keras.layers.Normalization` layer.
-norm_layer = layers.Normalization()
-# Fit the state of the layer to the spectrograms
-# with `Normalization.adapt`.
-norm_layer.adapt(data=spectrogram_ds.map(map_func=lambda spec, label: spec))
+merge_data = np.concatenate((Normal_0_X097, B007_0_X122, IR007_0_X122, OR007_3_0_X122, OR007_6_0_X122, OR007_12_0_X122))
+print(merge_data.shape)
