cc0722b33e4ac76a8226812439b40da47ad8a7d8
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-01-11 21:10:44 +0700

    Update train_cnn.py

--- train_cnn.py
+++ train_cnn.py
@@ -20,15 +20,17 @@ def train(data=None, labels=None,\
   _, cnn_train_acc, cnn_train_f1_m, cnn_train_precision_m, cnn_train_recall_m = model.evaluate(data, labels, verbose=0)
   _, cnn_test_acc, cnn_test_f1_m, cnn_test_precision_m, cnn_test_recall_m = model.evaluate(val_data, val_labels, verbose=0)
 
-  # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/cnn_train_acc.npy', cnn_train_acc)
-  # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/cnn_train_f1_m.npy', cnn_train_f1_m)
-  # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/cnn_train_precision_m.npy', cnn_train_precision_m)
-  # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/cnn_train_recall_m.npy', cnn_train_recall_m)
+  np.save('/content/drive/Shareddrives/newpro112233/signal_machine/evaluate/cnn_history.npy', history)
 
-  # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/cnn_test_acc.npy', cnn_test_acc)
-  # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/cnn_test_f1_m.npy', cnn_test_f1_m)
-  # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/cnn_test_precision_m.npy', cnn_test_precision_m)
-  # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/cnn_test_recall_m.npy', cnn_test_recall_m)
+  np.save('/content/drive/Shareddrives/newpro112233/signal_machine/evaluate/cnn_train_acc.npy', cnn_train_acc)
+  np.save('/content/drive/Shareddrives/newpro112233/signal_machine/evaluate/cnn_train_f1_m.npy', cnn_train_f1_m)
+  np.save('/content/drive/Shareddrives/newpro112233/signal_machine/evaluate/cnn_train_precision_m.npy', cnn_train_precision_m)
+  np.save('/content/drive/Shareddrives/newpro112233/signal_machine/evaluate/cnn_train_recall_m.npy', cnn_train_recall_m)
+
+  np.save('/content/drive/Shareddrives/newpro112233/signal_machine/evaluate/cnn_test_acc.npy', cnn_test_acc)
+  np.save('/content/drive/Shareddrives/newpro112233/signal_machine/evaluate/cnn_test_f1_m.npy', cnn_test_f1_m)
+  np.save('/content/drive/Shareddrives/newpro112233/signal_machine/evaluate/cnn_test_precision_m.npy', cnn_test_precision_m)
+  np.save('/content/drive/Shareddrives/newpro112233/signal_machine/evaluate/cnn_test_recall_m.npy', cnn_test_recall_m)
   print('Train: %.3f, Test: %.3f' % (cnn_train_acc, cnn_test_acc))
   # plot training history
   pyplot.plot(history.history['loss'], label='train')
