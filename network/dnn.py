f20e4b147e125675ca7a1a431f32051869d13e62
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-01-11 18:38:11 +0700

    Update dnn.py

--- network/dnn.py
+++ network/dnn.py
@@ -9,19 +9,12 @@ network+None | merge_network+None | merge_network+Fourier  | merge_network+SVD_d
 40%           93%                   ~100% (After 6 epochs)  ~90%
 '''
 
-def network():
-  # Build neural network
-  
-  return model
-
-def merge_network(use_network=False, use_Wavelet=False, use_Fourier=False, use_Wavelet_denoise=False, use_SVD=False, use_savitzky_golay=False, none=False):
+def network(use_network=False, use_Wavelet=False, use_Fourier=False, use_Wavelet_denoise=False, use_SVD=False, use_savitzky_golay=False, none=False):
   if use_network:
     input_ = keras.layers.Input(shape=[400, ])
-    hidden3 = keras.layers.Dense(300, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="lecun_normal")(input_)
-    norm3 = keras.layers.BatchNormalization()(hidden3)
-    hidden4 = keras.layers.Dense(100, activation=tf.keras.layers.ReLU(), kernel_regularizer='l1_l2', kernel_initializer="lecun_normal")(norm3)
-    norm4 = keras.layers.BatchNormalization()(hidden4)
-    concat = keras.layers.concatenate([input_, norm4])
+    hidden3 = keras.layers.Dense(300, activation=tf.keras.layers.ReLU())(input_)
+    hidden4 = keras.layers.Dense(100, activation=tf.keras.layers.ReLU())(hidden3)
+    concat = keras.layers.concatenate([input_, hidden4])
     output = keras.layers.Dense(6, activation=tf.keras.layers.Softmax())(concat)
     model = keras.models.Model(inputs=[input_], outputs=[output])
   else:
@@ -31,9 +24,9 @@ def merge_network(use_network=False, use_Wavelet=False, use_Fourier=False, use_W
     if use_Fourier or use_Wavelet_denoise or use_SVD or use_savitzky_golay or none:
       input_A = keras.layers.Input(shape=[200], name="wide_input")
       input_B = keras.layers.Input(shape=[200], name="deep_input") 
-    hidden1 = keras.layers.Dense(30, activation="relu")(input_B)
-    hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+    hidden1 = keras.layers.Dense(300, activation=tf.keras.layers.ReLU())(input_B)
+    hidden2 = keras.layers.Dense(100, activation=tf.keras.layers.ReLU())(hidden1)
     concat = keras.layers.concatenate([input_A, hidden2])
-    output = keras.layers.Dense(6, name="output")(concat)
+    output = keras.layers.Dense(6, activation=tf.keras.layers.Softmax(), name="output")(concat)
     model = keras.models.Model(inputs=[input_A, input_B], outputs=[output])
   return model
