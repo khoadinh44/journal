fd7b2c4a1e2f037fa041af2677ae31f04f13ea55
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-01-11 18:38:52 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import numpy as np
-from network.dnn import network, merge_network
+from network.dnn import network
 from sklearn.model_selection import train_test_split
 from preprocessing.denoise_signal import signaltonoise_dB
-from preprecessing.utils import recall_m, precision_m, f1_m
+from preprocessing.utils import recall_m, precision_m, f1_m
 from load_data import label, use_network, use_Wavelet, use_Fourier, use_Wavelet_denoise, use_SVD, use_savitzky_golay, none
 
 use_network = False
@@ -41,15 +41,15 @@ if use_network:
     _, dnn_train_acc, dnn_train_f1_m, dnn_train_precision_m, dnn_train_recall_m = model.evaluate(data, labels, verbose=0)
     _, dnn_test_acc, dnn_test_f1_m, dnn_test_precision_m, dnn_test_recall_m = model.evaluate(val_data, val_labels, verbose=0)
 
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_train_acc.npy', dnn_train_acc)
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_train_f1_m.npy', dnn_train_f1_m)
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_train_precision_m.npy', dnn_train_precision_m)
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_train_recall_m.npy', dnn_train_recall_m)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_train_acc.npy', dnn_train_acc)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_train_f1_m.npy', dnn_train_f1_m)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_train_precision_m.npy', dnn_train_precision_m)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_train_recall_m.npy', dnn_train_recall_m)
 
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_test_acc.npy', dnn_test_acc)
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_test_f1_m.npy', dnn_test_f1_m)
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_test_precision_m.npy', dnn_test_precision_m)
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_test_recall_m.npy', dnn_test_recall_m)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_test_acc.npy', dnn_test_acc)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_test_f1_m.npy', dnn_test_f1_m)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_test_precision_m.npy', dnn_test_precision_m)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/dnn_test_recall_m.npy', dnn_test_recall_m)
     print('Train: %.3f, Test: %.3f' % (dnn_train_acc, dnn_test_acc))
 
     # pyplot.plot(history.history['loss'], label='train')
@@ -57,7 +57,7 @@ if use_network:
     # pyplot.legend()
     # pyplot.show()
 
-  train(X_train, y_train, X_test, y_test, merge_network, 100, 32, True, 'model.h5')
+  train(X_train, y_train, X_test, y_test, network, 10, 32, True, 'model.h5')
 
 else:
   def train(data=None, labels=None,\
@@ -74,19 +74,19 @@ else:
     _, best_train_acc, best_train_f1_m, best_train_precision_m, best_train_recall_m = model.evaluate(data, labels, verbose=0)
     _, best_test_acc, best_test_f1_m, best_test_precision_m, best_test_recall_m = model.evaluate(val_data, val_labels, verbose=0)
 
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_train_acc.npy', best_train_acc)
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_train_f1_m.npy', best_train_f1_m)
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_train_precision_m.npy', best_train_precision_m)
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_train_recall_m.npy', best_train_recall_m)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_train_acc.npy', best_train_acc)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_train_f1_m.npy', best_train_f1_m)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_train_precision_m.npy', best_train_precision_m)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_train_recall_m.npy', best_train_recall_m)
 
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_test_acc.npy', best_test_acc)
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_test_f1_m.npy', best_test_f1_m)
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_test_precision_m.npy', best_test_precision_m)
-    np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_test_recall_m.npy', best_test_recall_m)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_test_acc.npy', best_test_acc)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_test_f1_m.npy', best_test_f1_m)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_test_precision_m.npy', best_test_precision_m)
+    # np.save('/content/drive/Shareddrives/newpro112233/signal_machine/best_test_recall_m.npy', best_test_recall_m)
     print('Train: %.3f, Test: %.3f' % (best_train_acc, best_test_acc))
 
     # pyplot.plot(history.history['loss'], label='train')
     # pyplot.plot(history.history['val_loss'], label='test')
     # pyplot.legend()
     # pyplot.show()
-  train((X_train_A, X_train_B), y_train, (X_test_A, X_test_B), y_test, merge_network, 100, 32, True, 'model.h5')
+  train((X_train_A, X_train_B), y_train, (X_test_A, X_test_B), y_test, network, 10, 32, True, 'model.h5')
