29a3a94a83f3f3ca2d99832858fd7eeaa82976cf
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-02-06 11:21:34 +0700

    Update utils.py

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -127,3 +127,11 @@ def convert_one_hot(x):
         index = idx
     return [index]
 
+def invert_one_hot(x, num_class):
+    all_labels = []
+    label_zeros = np.zeros((num_class, ))
+    for i in x:
+        label = np.copy(label_zeros)
+        label[i] = 1
+        all_labels.append(label)
+    return np.array(all_labels)
