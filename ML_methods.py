cd9678e14c5281936b6d80fb415eb6259e2547ab
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-02-06 17:00:09 +0700

    Update ML_methods.py

--- ML_methods.py
+++ ML_methods.py
@@ -49,7 +49,7 @@ def get_data(opt):
     OR007_3_0_label_all = convert_one_hot(OR007_3_0_label) * OR007_3_0.shape[0]
     OR007_12_0_label_all = convert_one_hot(OR007_12_0_label) * OR007_12_0.shape[0]
     all_labels_0 = np.concatenate((Normal_0_label_all, IR007_0_label_all, B007_0_label_all, OR007_6_0_label_all, OR007_3_0_label_all, OR007_12_0_label_all))
-    X_train, X_test, y_train, y_test = train_test_split(all_data_0, all_labels_0, test_size=opt.test_rate, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(all_data_0, all_labels_0, test_size=opt.test_rate, random_state=42, shuffle=True)
 
   if opt.case_1_7:
     all_data_1 = np.concatenate((Normal_1, IR007_1, B007_1, OR007_6_1, OR007_3_1, OR007_12_1))
@@ -60,7 +60,7 @@ def get_data(opt):
     OR007_3_1_label_all = convert_one_hot(OR007_3_1_label) * OR007_3_1.shape[0]
     OR007_12_1_label_all = convert_one_hot(OR007_12_1_label) * OR007_12_1.shape[0]
     all_labels_1 = np.concatenate((Normal_1_label_all, IR007_1_label_all, B007_1_label_all, OR007_6_1_label_all, OR007_3_1_label_all, OR007_12_1_label_all))
-    X_train, X_test, y_train, y_test = train_test_split(all_data_1, all_labels_1, test_size=opt.test_rate, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(all_data_1, all_labels_1, test_size=opt.test_rate, random_state=42, shuffle=True)
 
   if opt.case_2_8:
     all_data_2 = np.concatenate((Normal_2, IR007_2, B007_2, OR007_6_2, OR007_3_2, OR007_12_2))
@@ -71,7 +71,7 @@ def get_data(opt):
     OR007_3_2_label_all = convert_one_hot(OR007_3_2_label) * OR007_3_2.shape[0]
     OR007_12_2_label_all = convert_one_hot(OR007_12_2_label) * OR007_12_2.shape[0]
     all_labels_2 = np.concatenate((Normal_2_label_all, IR007_2_label_all, B007_2_label_all, OR007_6_2_label_all, OR007_3_2_label_all, OR007_12_2_label_all))
-    X_train, X_test, y_train, y_test = train_test_split(all_data_2, all_labels_2, test_size=opt.test_rate, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(all_data_2, all_labels_2, test_size=opt.test_rate, random_state=42, shuffle=True)
 
   if opt.case_3_9:
     all_data_3 = np.concatenate((Normal_3, IR007_3, B007_3, OR007_6_3, OR007_3_3, OR007_12_3))
@@ -82,12 +82,12 @@ def get_data(opt):
     OR007_3_3_label_all = convert_one_hot(OR007_3_3_label) * OR007_3_3.shape[0]
     OR007_12_3_label_all = convert_one_hot(OR007_12_3_label) * OR007_12_3.shape[0]
     all_labels_3 = np.concatenate((Normal_3_label_all, IR007_3_label_all, B007_3_label_all, OR007_6_3_label_all, OR007_3_3_label_all, OR007_12_3_label_all))
-    X_train, X_test, y_train, y_test = train_test_split(all_data_3, all_labels_3, test_size=opt.test_rate, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(all_data_3, all_labels_3, test_size=opt.test_rate, random_state=42, shuffle=True)
 
   if opt.case_4_10:
     all_data_4 = np.concatenate((all_data_0, all_data_1, all_data_2, all_data_3))
     all_labels_4 = np.concatenate((all_labels_0, all_labels_1, all_labels_2, all_labels_3))
-    X_train, X_test, y_train, y_test = train_test_split(all_data_4, all_labels_4, test_size=opt.test_rate, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(all_data_4, all_labels_4, test_size=opt.test_rate, random_state=42, shuffle=True)
 
   if opt.case_5_11:
     Normal_5 = np.concatenate((Normal_0, Normal_1, Normal_2, Normal_3))
@@ -107,7 +107,7 @@ def get_data(opt):
     OR007_12_5_label_all = convert_one_hot(OR007_12_0_label) * OR007_12_5.shape[0]
     all_labels_5 = np.concatenate((Normal_5_label_all, IR007_5_label_all, B007_5_label_all, OR007_6_5_label_all, OR007_3_5_label_all, OR007_12_5_label_all))
 
-    X_train, X_test, y_train, y_test = train_test_split(all_data_5, all_labels_5, test_size=opt.test_rate, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(all_data_5, all_labels_5, test_size=opt.test_rate, random_state=42, shuffle=True)
   
   if opt.case_12:
     all_data_12 = np.concatenate((all_data_4, IR021_0, B021_0, OR021_6_0, OR021_3_0, OR021_12_0,
@@ -143,7 +143,7 @@ def get_data(opt):
                                     IR021_1_label_all, B021_1_label_all, OR021_6_1_label_all, OR021_3_1_label_all, OR021_12_1_label_all,
                                     IR021_2_label_all, B021_2_label_all, OR021_6_2_label_all, OR021_3_2_label_all, OR021_12_2_label_all,
                                     IR021_3_label_all, B021_3_label_all, OR021_6_3_label_all, OR021_3_3_label_all, OR021_12_3_label_all))
-    X_train, X_test, y_train, y_test = train_test_split(all_data_12, all_labels_12, test_size=opt.test_rate, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(all_data_12, all_labels_12, test_size=opt.test_rate, random_state=42, shuffle=True)
   
   if opt.case_13:
     IR021_13    = np.concatenate((IR007_5, IR021_0, IR021_1, IR021_2, IR021_3))
@@ -161,7 +161,7 @@ def get_data(opt):
     OR021_12_13_label_all = convert_one_hot(OR007_12_0_label) * OR021_12_13.shape[0]
     all_labels_13 = np.concatenate((Normal_5_label_all, IR021_13_label_all, B021_13_label_all, OR021_6_13_label_all, OR021_3_13_label_all, OR021_12_13_label_all))
 
-    X_train, X_test, y_train, y_test = train_test_split(all_data_13, all_labels_13, test_size=opt.test_rate, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(all_data_13, all_labels_13, test_size=opt.test_rate, random_state=42, shuffle=True)
   
   if opt.case_14:
     all_data_14 = np.concatenate((all_data_12, IR014_0, IR014_1, IR014_2, IR014_3,
@@ -201,13 +201,13 @@ def get_data(opt):
                                     IR028_0_label_all,   IR028_1_label_all,   IR028_2_label_all,   IR028_3_label_all,
                                     B028_0_label_all,    B028_1_label_all,    B028_2_label_all,    B028_3_label_all))
  
-    X_train, X_test, y_train, y_test = train_test_split(all_data_14, all_labels_14, test_size=opt.test_rate, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(all_data_14, all_labels_14, test_size=opt.test_rate, random_state=42, shuffle=True)
   
   if opt.case_15:
     all_data_15 = handcrafted_features(all_data_14)
     all_labels_15 = all_labels_14
     
-    X_train, X_test, y_train, y_test = train_test_split(all_data_15, all_labels_15, test_size=opt.test_rate, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(all_data_15, all_labels_15, test_size=opt.test_rate, random_state=42, shuffle=True)
   
   if opt.use_ML:
     model = SVC(kernel='rbf', probability=True)
@@ -218,7 +218,7 @@ def get_data(opt):
   
     test_predictions = model.predict(X_test)
     print("Accuracy:", accuracy_score(y_test, test_predictions))
-    
+
   return X_train, X_test, y_train, y_test
   
 def parse_opt(known=False):
