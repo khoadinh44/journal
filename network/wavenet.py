842cce770e73697e2420680b6c5c20890eaf3b61
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-02-09 18:49:50 +0700

    Update wavenet.py

--- network/wavenet.py
+++ network/wavenet.py
@@ -8,7 +8,7 @@ from .module import Conv1D, ReLU, ResidualConv1DGLU
 from .upsample import UpsampleNetwork
 # Reference:  https://github.com/kokeshing/WaveNet-tf2
 
-def WaveNet(num_classes):
+def WaveNet(num_classes, opt):
     inputs = Input(shape=[400, 1])
     x = Conv1D(128, kernel_size=1, padding='causal')(inputs)
 
@@ -29,35 +29,12 @@ def WaveNet(num_classes):
 
     x = MaxPooling1D(pool_size=4, strides=None)(x)
     x = GlobalAveragePooling1D()(x)
-    
-    x = Dense(num_classes, activation='softmax')(x)
-
-    m = Model(inputs, x, name='wavenet')
+    if opt.multi_head == True:
+      x = TransformerLayer(x=x, c=256)
+      x = Dense(num_classes, activation='softmax')(x)
+      m = Model(inputs, x, name='wavenet_multi_head')
+    else:
+      x = Dense(num_classes, activation='softmax')(x)
+      m = Model(inputs, x, name='wavenet')
     return m
 
-def WaveNet_Head(num_classes):
-    inputs = Input(shape=[400, 1])
-    x = Conv1D(128, kernel_size=1, padding='causal')(inputs)
-
-    skips = None
-    for _ in range(2):
-        for i in range(10):
-            x, h = ResidualConv1DGLU(128, 256, kernel_size=3, skip_out_channels=128, dilation_rate=2 ** i)(x)
-            if skips is None:
-                skips = h
-            else:
-                skips = skips + h
-    x = skips
-    x = tf.keras.layers.ReLU()(x)
-
-    x = Conv1D(128, kernel_size=1, padding='causal')(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = Conv1D(256, kernel_size=1, padding='causal')(x)
-
-    x = MaxPooling1D(pool_size=4, strides=None)(x)
-    x = GlobalAveragePooling1D()(x)
-    x = TransformerLayer(x)
-    x = Dense(num_classes, activation='softmax')(x)
-
-    m = Model(inputs, x, name='wavenet multi head')
-    return m
