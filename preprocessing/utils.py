61567e8ec04c3549d401c00a870a1a85052bb03f
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-02-14 11:44:30 +0700

    Update utils.py

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -10,7 +10,7 @@ def add_noise(signal, SNRdb):
   signal_diff = signal - mean_S
   var_S = np.sum(np.mean(signal_diff**2)) 
 
-  mean_N = mean_S
+  mean_N = 0
   std = np.sqrt(var_S/np.power(10, (SNRdb/10)))
   noise = np.random.normal(mean_N, std, size=len(signal))
   noise_signal = signal + noise
