3c4aaa46118e6c81a3efbcb1355809efbc06f953
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-02-16 19:45:04 +0700

    Update enssemble.py

--- network/enssemble.py
+++ network/enssemble.py
@@ -1,4 +1,5 @@
 from tensorflow import keras
+import numpy as np
 from preprocessing.utils import accuracy_m
 
 def semble_transfer(opt, X_test, y_test):
@@ -6,7 +7,8 @@ def semble_transfer(opt, X_test, y_test):
   l = 0
   
   for name in opt.model_names:
-    all_path = opt.model_dir + name + '.hdf5'
+    all_path = opt.model_dir + name + '.h5'
+    model = keras.models.load_model(all_path)
     curr_y_pred = model.predict(X_test)
     keras.backend.clear_session()
     np.save(opt.model_dir + name + '.npy', curr_y_pred)
