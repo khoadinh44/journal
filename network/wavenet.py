4e4846ab1cb9cb78249aaa3d2ffba4819107152c
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-02-17 14:07:52 +0700

    Update wavenet.py

--- network/wavenet.py
+++ network/wavenet.py
@@ -9,7 +9,7 @@ from .module import Conv1D, ReLU, ResidualConv1DGLU
 from .upsample import UpsampleNetwork
 # Reference:  https://github.com/kokeshing/WaveNet-tf2
 
-def WaveNet(num_classes, opt):
+def WaveNet(opt):
     inputs = Input(shape=[400, 1])
     x = Conv1D(128, kernel_size=1, padding='causal')(inputs)
 
@@ -32,10 +32,9 @@ def WaveNet(num_classes, opt):
     x = GlobalAveragePooling1D()(x)
     if opt.multi_head == True:
       x = TransformerLayer(x=x, c=256)
-      x = Dense(num_classes, activation='softmax')(x)
+      x = Dense(opt.num_classes, activation='softmax')(x)
       m = Model(inputs, x, name='wavenet_multi_head')
     else:
-      x = Dense(num_classes, activation='softmax')(x)
+      x = Dense(opt.num_classes, activation='softmax')(x)
       m = Model(inputs, x, name='wavenet')
     return m
-
