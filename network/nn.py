19f39f78283b31cf5d1bba5ed97d5408ae3f2f61
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-02-25 06:15:44 +0700

    Update nn.py

--- network/nn.py
+++ network/nn.py
@@ -9,14 +9,35 @@ from keras import regularizers
 from keras.layers import Activation, BatchNormalization, Conv1D, Dense, GlobalAveragePooling1D, Input, MaxPooling1D, Lambda
 from keras.models import Model
 
+# def TransformerLayer(x=None, c=48, num_heads=4):
+#     # Transformer layer https://arxiv.org/abs/2010.11929 (LayerNorm layers removed for better performance)
+#     q   = Dense(c, use_bias=False)(x)
+#     k   = Dense(c, use_bias=False)(x)
+#     v   = Dense(c, use_bias=False)(x)
+#     ma  = MultiHeadAttention(head_size=c, num_heads=num_heads)([q, k, v]) + x
+#     fc1 = Dense(c, use_bias=False)(ma)                            
+#     fc2 = Dense(c, use_bias=False)(fc1) + x 
+#     return fc2
+
 def TransformerLayer(x=None, c=48, num_heads=4):
     # Transformer layer https://arxiv.org/abs/2010.11929 (LayerNorm layers removed for better performance)
-    q   = Dense(c, use_bias=False)(x)
-    k   = Dense(c, use_bias=False)(x)
-    v   = Dense(c, use_bias=False)(x)
+    a = tf.keras.layers.LayerNormalization()(x)
+    q   = Dense(c, use_bias=False)(a)
+    k   = Dense(c, use_bias=False)(a)
+    v   = Dense(c, use_bias=False)(a)
     ma  = MultiHeadAttention(head_size=c, num_heads=num_heads)([q, k, v]) + x
-    fc1 = Dense(c, use_bias=False)(ma)                            
-    fc2 = Dense(c, use_bias=False)(fc1) + x 
+    ma = tf.keras.layers.Dropout(0.1)(ma)
+
+    a2 = tf.keras.layers.LayerNormalization()(ma) 
+    fc1 = Dense(300, kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+                                  bias_regularizer=regularizers.l2(1e-4),
+                                  activity_regularizer=regularizers.l2(1e-5))(a2) 
+    fc1 = Activation('relu')(fc1)
+    fc1 = tf.keras.layers.Dropout(0.1)(fc1)                    
+    fc2 = concatenate([Dense(100, kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+                                  bias_regularizer=regularizers.l2(1e-4),
+                                  activity_regularizer=regularizers.l2(1e-5))(fc1), ma])
+    fc2 = tf.keras.layers.Dropout(0.1)(fc2)
     return fc2
 
 # For m34 Residual, use RepeatVector. Or tensorflow backend.repeat
@@ -137,7 +158,7 @@ def CNN_C(opt):
     https://github.com/philipperemy/very-deep-convnets-raw-waveforms/blob/master/model_resnet.py
     '''
     inputs = Input(shape=[400, 1])
-    x = Conv1D(opt.num_classes*2,
+    x = Conv1D(48,
                kernel_size=80,
                strides=4,
                padding='same',
@@ -148,12 +169,12 @@ def CNN_C(opt):
     x = MaxPooling1D(pool_size=4, strides=None)(x)
 
     for i in range(3):
-        x = identity_block(x, kernel_size=3, filters=opt.num_classes*2, stage=1, block=i)
+        x = identity_block(x, kernel_size=3, filters=48, stage=1, block=i)
 
     x = MaxPooling1D(pool_size=4, strides=None)(x)
     x = GlobalAveragePooling1D()(x)
     
-    x = TransformerLayer(x, c=opt.num_classes*2)
+    x = TransformerLayer(x, c=48)
     x = Dense(opt.num_classes, activation='softmax')(x)
 
     m = Model(inputs, x, name='resnet34')
