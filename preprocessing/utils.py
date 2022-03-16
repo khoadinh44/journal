75c59b65120a1d2e8ac968a26981f1292a5eaf4e
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-03-16 16:37:57 +0700

    Update utils.py

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -133,8 +133,8 @@ def get_spectrogram(waveform):
 def convert_spectrogram(waveform):
   data = []
   for each_signal in waveform:
-    each = get_spectrogram(each_signal)
-    three_dims = np.expand_dims(cv2.merge((each, each, each)))
+    each = get_spectrogram(each_signal).numpy()
+    three_dims = np.expand_dims(cv2.merge((each, each, each)), axis=0)
     if data == []:
       data = three_dims
     else:
