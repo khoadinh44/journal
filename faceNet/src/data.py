f4d88a0a0e7608a498c2ae0b5d49be41720037c3
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-03-21 15:40:52 +0700

    Update data.py

--- faceNet/src/data.py
+++ faceNet/src/data.py
@@ -6,28 +6,6 @@ import tensorflow as tf
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 # tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
-'''
-Create tf.data.Dataset from a directory of Images.
-Preprocess Images : Resize, Flip, Normalize
-'''
-
-
-def preprocess_image(image, image_size):
-    
-    image = tf.image.resize(image, [image_size, image_size])
-    image = tf.image.random_flip_left_right(image)
-    image /= 255.0
-    return image
-
-
-def parse_image_function(image_path, label, image_size):
-      
-    image_string = tf.io.read_file(image_path)
-    image = tf.image.decode_jpeg(image_string, channels=3)
-    image = preprocess_image(image, image_size)
-    return image, label
-
-
 def get_dataset(data, labels, params, phase='train'):
 
     AUTOTUNE   =  tf.data.experimental.AUTOTUNE
