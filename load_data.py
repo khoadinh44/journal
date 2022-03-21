c9ce8635bca8fb8ece71593d9d3d34ff124b81c0
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-03-21 18:52:59 +0700

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -12,7 +12,8 @@ from sklearn.preprocessing import RobustScaler
 from sklearn.preprocessing import Normalizer
 from sklearn.preprocessing import QuantileTransformer
 from sklearn.preprocessing import PowerTransformer
-from train import parse_opt
+# from train import parse_opt
+from faceNet import parse_opt
 
 opt = parse_opt()
 
