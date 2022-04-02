139b1489678d1fd347be4206199008080ea470b8
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-02 21:14:11 +0700

    Update faceNet.py

--- faceNet.py
+++ faceNet.py
@@ -178,7 +178,7 @@ def parse_opt(known=False):
 
     # Parameters---------------------------------------------
     parser.add_argument('--save',            type=str,   default='/content/drive/Shareddrives/newpro112233/signal_machine/', help='Position to save weights')
-    parser.add_argument('--num_classes',     type=int,   default=128,         help='Number of classes')
+    parser.add_argument('--num_classes',     type=int,   default=3,         help='128 Number of classes in faceNet')
     parser.add_argument('--input_shape',     type=int,   default=255900,        help='255900 in 5-fold or 250604 in the only training.')
     parser.add_argument('--batch_size',      type=int,   default=64,         help='Number of batch size for training')
     parser.add_argument('--test_rate',       type=float, default=0.2,        help='rate of split data for testing')
