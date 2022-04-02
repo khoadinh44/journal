1408bf385aae34f4ababb98c4a6ba1790452808c
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-02 16:17:37 +0700

    Update 5_fold.py

--- 5_fold.py
+++ 5_fold.py
@@ -1,7 +1,7 @@
 from faceNet import Trainer, parse_opt
 from FaceNet_predict import FaceNetOneShotRecognitor
 from load_data import Healthy, Outer_ring_damage, Inner_ring_damage 
-from preprocessing.utils import invert_one_hot
+from preprocessing.utils import invert_one_hot, load_table_10_spe
 from src.params import Params
 
 from src.data import get_dataset
@@ -21,24 +21,21 @@ from sklearn.metrics import accuracy_score
 opt = parse_opt()
 
 print('\t\t\t Loading labels...')
-Healthy_label = [0]*len(Healthy)
-Outer_ring_damage_label = [1]*len(Outer_ring_damage)
-Inner_ring_damage_label = [2]*len(Inner_ring_damage)
 accuracy = []
 
 for i in range(len(Healthy)):
   X_train_Healthy = Healthy[i: i+3]
-  y_train_Healthy = Healthy_label[i: i+3]
+  y_train_Healthy = [0]*len(X_train_Healthy)
   X_train_Healthy, y_train_Healthy = load_table_10_spe(X_train_Healthy, y_train_Healthy)
   print(f'\n Shape of the Health train data and label: {X_train_Healthy.shape}, {y_train_Healthy.shape}')
   
   X_train_Outer_ring_damage = Outer_ring_damage[i: i+3]
-  y_train_Outer_ring_damage = Outer_ring_damage_label[i: i+3]
+  y_train_Outer_ring_damage = [1]*len(X_train_Outer_ring_damage)
   X_train_Outer_ring_damage, y_train_Outer_ring_damage = load_table_10_spe(X_train_Outer_ring_damage, y_train_Outer_ring_damage)
   print(f'\n Shape of the Outer ring damage train data and label: {X_train_Outer_ring_damage.shape}, {y_train_Outer_ring_damage.shape}')
   
   X_train_Inner_ring_damage = Inner_ring_damage[i: i+3]
-  y_train_Inner_ring_damage = Inner_ring_damage_label[i: i+3]
+  y_train_Inner_ring_damage = [2]*len(X_train_Inner_ring_damage)
   X_train_Inner_ring_damage, y_train_Inner_ring_damage = load_table_10_spe(X_train_Inner_ring_damage, y_train_Inner_ring_damage)
   print(f'\n Shape of the Inner ring damage train data and label: {X_train_Inner_ring_damage.shape}, {y_train_Inner_ring_damage.shape}')
   
@@ -49,18 +46,22 @@ for i in range(len(Healthy)):
     break
   
   print('\n------------------------------------------------')
-  X_test_Healthy = [i for i in Healthy if i not in X_train_Healthy]
-  y_test_Healthy = [i for i in Healthy_label if i not in y_train_Healthy]
+
+  h = [a for a in range(len(Healthy)) if a not in range(i, i+3)]
+  X_test_Healthy = Healthy[h]
+  y_test_Healthy = [0]*len(X_test_Healthy)
   X_test_Healthy, y_test_Healthy = load_table_10_spe(X_test_Healthy, y_test_Healthy)
   print(f'\n Shape of the Health test data and label: {X_test_Healthy.shape}, {y_test_Healthy.shape}')
   
-  X_test_Outer_ring_damage = [i for i in Outer_ring_damage if i not in X_train_Outer_ring_damage]
-  y_test_Outer_ring_damage = [i for i in Outer_ring_damage_label if i not in y_train_Outer_ring_damage]
+  k = [a for a in range(len(Outer_ring_damage)) if a not in range(i, i+3)]
+  X_test_Outer_ring_damage = Outer_ring_damage[k]
+  y_test_Outer_ring_damage = [1]*len(X_test_Outer_ring_damage)
   X_test_Outer_ring_damage, y_test_Outer_ring_damage = load_table_10_spe(X_test_Outer_ring_damage, y_test_Outer_ring_damage)
   print(f'\n Shape of the Outer ring damage test data and label: {X_test_Outer_ring_damage.shape}, {y_test_Outer_ring_damage.shape}')
   
-  X_test_Inner_ring_damage = [i for i in Inner_ring_damage if i not in X_train_Inner_ring_damage]
-  y_test_Inner_ring_damage = [i for i in Inner_ring_damage_label if i not in y_train_Inner_ring_damage]
+  l = [a for a in range(len(Inner_ring_damage)) if a not in range(i, i+3)]
+  X_test_Inner_ring_damage = Inner_ring_damage[l]
+  y_test_Inner_ring_damage = [2]*len(X_test_Inner_ring_damage)
   X_test_Inner_ring_damage, y_test_Inner_ring_damage = load_table_10_spe(X_test_Inner_ring_damage, y_test_Inner_ring_damage)
   print(f'\n Shape of the Inner ring damage test data and label: {X_test_Inner_ring_damage.shape}, {y_test_Inner_ring_damage.shape}')
   
@@ -69,7 +70,7 @@ for i in range(len(Healthy)):
   print(f'\n Shape of test data: {X_test.shape}, {y_test.shape}')
   
   print('\n Train phase...')
-  trainer = Trainer(opt)
+  trainer = Trainer(opt, X_train, X_test, y_train, y_test)
   for i in range(opt.epoch):
       trainer.train(i)
   
