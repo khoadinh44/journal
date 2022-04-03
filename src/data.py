9a63a68fe9e81103b86a70b70563539d69fb14c2
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-03 11:32:28 +0700

    Update data.py

--- src/data.py
+++ src/data.py
@@ -3,19 +3,12 @@ import numpy as np
 from preprocessing.utils import invert_one_hot
 import tensorflow as tf
 
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-
 def get_dataset(data, labels, params, phase='train'):
     if len(np.array(labels).shape) > 1:
       labels = invert_one_hot(labels)
-    data = np.expand_dims(data, axis=-1)
+      
     AUTOTUNE   =  tf.data.experimental.AUTOTUNE
     dataset    =  tf.data.Dataset.from_tensor_slices((data, labels))
     dataset    =  dataset.batch(params.batch_size).prefetch(AUTOTUNE)
     
     return dataset, int(data.shape[0])
-
-
-if __name__ == "__main__":
-    print(get_dataset('../data'))
