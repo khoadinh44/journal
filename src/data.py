5d0e60a40e3c5133a6967c3daf58a8186d823336
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-03 11:33:53 +0700

    Update data.py

--- src/data.py
+++ src/data.py
@@ -4,11 +4,10 @@ from preprocessing.utils import invert_one_hot
 import tensorflow as tf
 
 def get_dataset(data, labels, params, phase='train'):
-    if len(np.array(labels).shape) > 1:
-      labels = invert_one_hot(labels)
-      
     AUTOTUNE   =  tf.data.experimental.AUTOTUNE
     dataset    =  tf.data.Dataset.from_tensor_slices((data, labels))
     dataset    =  dataset.batch(params.batch_size).prefetch(AUTOTUNE)
     
     return dataset, int(data.shape[0])
+
+
