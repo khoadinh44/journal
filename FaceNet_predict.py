3bec0a48f1e71e6d76c304a8ecbccb7971117c67
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-04 09:28:20 +0700

    Update FaceNet_predict.py

--- FaceNet_predict.py
+++ FaceNet_predict.py
@@ -70,11 +70,8 @@ class FaceNetOneShotRecognitor(object):
     def loading(self, input_):
         all_data = []
         for i in input_:
-            if all_data == []:
-                all_data = i
-            else:
-                all_data = np.concatenate((all_data, i))
-        return all_data
+            all_data.append(i)
+        return np.array(all_data)
       
     def train_or_load(self, cons=True):        
         for ID, (train_data, train_label) in enumerate(zip(self.X_train_all, self.y_train_all)):
@@ -120,7 +117,7 @@ class FaceNetOneShotRecognitor(object):
         else:
           train_label = self.df_train['name']
           train_label = self.loading(train_label)
-          print(train_label)
+          # print(train_label)
           if opt.ML_method == 'SVM':
             model = SVC(kernel='rbf', probability=True)
           elif opt.ML_method == 'RandomForestClassifier':
