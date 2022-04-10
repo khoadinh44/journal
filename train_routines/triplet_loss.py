de6fb3eb5090c1f44c89b6fc64a9ed5ec952f31b
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-10 08:54:50 +0700

    Update triplet_loss.py

--- train_routines/triplet_loss.py
+++ train_routines/triplet_loss.py
@@ -25,11 +25,15 @@ def train(opt, x_train, y_train, x_test, y_test, network):
 
     model_input = Input(shape=(opt.input_shape, 1))
     softmax, pre_logits = network(opt, model_input)
+    
+    tf.keras.backend.clear_session()
+    tf.compat.v1.reset_default_graph()
+
     shared_model = tf.keras.models.Model(inputs=[model_input], outputs=[softmax, pre_logits])
     # shared_model.summary()
    
     X_train, Y_train = generate_triplet(x_train, y_train)  #(anchors, positive, negative)
-    X_test, Y_test = generate_triplet(x_test, y_test)
+    # X_test, Y_test = generate_triplet(x_test, y_test)
   
     anchor_input = Input((opt.input_shape, 1,), name='anchor_input')
     positive_input = Input((opt.input_shape, 1,), name='positive_input')
@@ -43,7 +47,10 @@ def train(opt, x_train, y_train, x_test, y_test, network):
     merged_soft = concatenate([soft_anchor, soft_pos, soft_neg], axis=-1, name='merged_soft')
     
     loss_weights = [1, 0.01]
-   
+
+    tf.keras.backend.clear_session()
+    tf.compat.v1.reset_default_graph()
+
     model = Model(inputs=[anchor_input, positive_input, negative_input], outputs=[merged_soft, merged_pre])
     if os.path.isfile(outdir + "triplet_loss_model.h5"):
       model.load_weights(outdir + "triplet_loss_model.h5")
@@ -59,27 +66,27 @@ def train(opt, x_train, y_train, x_test, y_test, network):
     positive = X_train[:, 1, :].reshape(-1, opt.input_shape, 1)
     negative = X_train[:, 2, :].reshape(-1, opt.input_shape, 1)
 
-    anchor_t   = X_test[:, 0, :].reshape(-1, opt.input_shape, 1)
-    positive_t = X_test[:, 1, :].reshape(-1, opt.input_shape, 1)
-    negative_t = X_test[:, 2, :].reshape(-1, opt.input_shape, 1)
+    # anchor_t   = X_test[:, 0, :].reshape(-1, opt.input_shape, 1)
+    # positive_t = X_test[:, 1, :].reshape(-1, opt.input_shape, 1)
+    # negative_t = X_test[:, 2, :].reshape(-1, opt.input_shape, 1)
 
     y_anchor   = to_one_hot(Y_train[:, 0])
     y_positive = to_one_hot(Y_train[:, 1])
     y_negative = to_one_hot(Y_train[:, 2])
 
-    y_anchor_t   = to_one_hot(Y_test[:, 0])
-    y_positive_t = to_one_hot(Y_test[:, 1])
-    y_negative_t = to_one_hot(Y_test[:, 2])
+    # y_anchor_t   = to_one_hot(Y_test[:, 0])
+    # y_positive_t = to_one_hot(Y_test[:, 1])
+    # y_negative_t = to_one_hot(Y_test[:, 2])
 
     target = np.concatenate((y_anchor, y_positive, y_negative), -1)
-    target_t = np.concatenate((y_anchor_t, y_positive_t, y_negative_t), -1)
+    # target_t = np.concatenate((y_anchor_t, y_positive_t, y_negative_t), -1)
 
-    test_data = [anchor_t, positive_t, negative_t]
-    test_label = [target_t, target_t]
+    # test_data = [anchor_t, positive_t, negative_t]
+    # test_label = [target_t, target_t]
 
     # Fit data-------------------------------------------------
     model.fit(x=[anchor, positive, negative], y=[target, target],
-              batch_size=opt.batch_size, epochs=opt.epoch, callbacks=[TensorBoard(log_dir=outdir)], validation_data=(test_data, test_label))
+              batch_size=opt.batch_size, epochs=opt.epoch, callbacks=[TensorBoard(log_dir=outdir)])
     model.save(outdir + "triplet_loss_model.h5")
 
     # Embedding------------------------------------------------
