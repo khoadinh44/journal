078442e3cfed3788b26de4b3c34294a389420bb7
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-11 19:15:21 +0700

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -14,7 +14,7 @@ from sklearn.preprocessing import Normalizer
 from sklearn.preprocessing import QuantileTransformer
 from sklearn.preprocessing import PowerTransformer
 # from train import parse_opt
-from train_routines.triplet_loss import parse_opt
+from train import parse_opt
 
 opt = parse_opt()
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
