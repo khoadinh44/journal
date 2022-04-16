66dfa99fe50277594ecd752d25c94e263e726414
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-16 13:22:13 +0700

    Update center_loss.py

--- train_routines/center_loss.py
+++ train_routines/center_loss.py
@@ -62,10 +62,10 @@ def train_center_loss(opt, x_train, y_train, x_test, y_test, network):
 
     model.save(outdir + "center_loss_model.h5")
 
-    model = Model(inputs=[x_input, target_input], outputs=[softmax, l2_loss, pre_logits])
+    model = Model(inputs=[x_input], outputs=[softmax, merged_pre])
     model.load_weights(outdir + "center_loss_model.h5")
 
-    _, _, X_train_embed          = model.predict([x_train, y_train])
-    y_test_soft, _, X_test_embed = model.predict([x_test, y_test])
+    _, X_train_embed  = model.predict([x_train, y_train])
+    y_test_soft, X_test_embed   = model.predict([x_test, y_test])
 
     return X_train_embed, X_test_embed, y_test_soft
