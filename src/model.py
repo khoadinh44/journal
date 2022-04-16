88c4da97458c2d57e2d601504e3c461f7656e2f0
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-16 13:50:16 +0700

    Update model.py

--- src/model.py
+++ src/model.py
@@ -5,10 +5,10 @@ import keras
 from tensorflow_addons.layers import MultiHeadAttention
 from tensorflow.keras.layers import Conv1D, Activation, Dense, concatenate
 import keras.backend as K
-from keras import layers
-from keras import regularizers
-from keras.layers import Activation, BatchNormalization, Conv1D, Dense, GlobalAveragePooling1D, Input, MaxPooling1D, Lambda
-from keras.models import Model
+from tensorflow.keras import layers
+from tensorflow.keras import regularizers
+from tensorflow.keras.layers import Activation, BatchNormalization, Conv1D, Dense, GlobalAveragePooling1D, Input, MaxPooling1D, Lambda
+from tensorflow.keras.models import Model
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
