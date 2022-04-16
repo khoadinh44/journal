4b518eac573b5ea00027f0d80984d7cbe44e3863
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-16 10:20:33 +0700

    Update center_loss.py

--- train_routines/center_loss.py
+++ train_routines/center_loss.py
@@ -10,6 +10,7 @@ from tensorflow.keras.layers import concatenate, Lambda, Embedding, Input
 import tensorflow.keras.backend as K
 import numpy as np
 from tensorflow.keras.callbacks import TensorBoard
+from angular_grad import AngularGrad
 import os
 import argparse
 
@@ -26,16 +27,17 @@ def train_center_loss(opt, x_train, y_train, x_test, y_test, network):
     x_input = Input(shape=(opt.input_shape, 1))
     y_train_onehot = to_one_hot(y_train)
     
-    softmax, pre_logits = network(opt, model_input)
+    softmax, pre_logits = network(opt, x_input)
     target_input = Input((1,), name='target_input')
-    center = Embedding(10, embedding_size)(target_input)
+    center = Embedding(10, opt.embedding_size)(target_input)
     
     l2_loss = Lambda(lambda x: K.sum(K.square(x[0] - x[1][:, 0]), 1, keepdims=True), name='l2_loss')([pre_logits, center])
-
+    
+    tf.keras.backend.clear_session()
+    tf.compat.v1.reset_default_graph()
     model = tf.keras.models.Model(inputs=[x_input, target_input], outputs=[softmax, l2_loss])
-
     model.compile(loss=["categorical_crossentropy", lambda y_true, y_pred: y_pred],
-                  optimizer=tf.keras.optimizers.Adam(lr=lr), metrics=["accuracy"],
+                  optimizer=tf.keras.optimizers.Adam(), metrics=["accuracy"],
                   loss_weights=loss_weights)
 
     model.fit([x_train, y_train], y=[y_train_onehot, y_train],
