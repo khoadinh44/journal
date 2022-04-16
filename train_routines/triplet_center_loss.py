239a20aad9d14ed7896d262f0e1b3684d8fa2650
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-16 13:49:03 +0700

    Update triplet_center_loss.py

--- train_routines/triplet_center_loss.py
+++ train_routines/triplet_center_loss.py
@@ -7,6 +7,7 @@ import tensorflow.keras.backend as K
 import numpy as np
 from tensorflow.keras.callbacks import TensorBoard
 from angular_grad import AngularGrad
+from keras.layers import Dense
 import os
 import argparse
 
@@ -16,33 +17,45 @@ def train_triplet_center_loss(opt, x_train, y_train, x_test, y_test, network):
 
     outdir = opt.outdir + "/triplet_center_loss/"
 
+    x_input = Input(shape=(opt.input_shape, 1), name='x_input')
+    y_train_onehot = to_one_hot(y_train)
+
+    x_train = x_train.astype(np.float32)
+    x_test  = x_test.astype(np.float32)
+    y_train = y_train.astype(np.float32)
+    y_test  = y_test.astype(np.float32)
+
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
     loss_weights = [1, 0.1]
 
-    x_input = Input(shape=(opt.input_shape, 1))
     softmax, pre_logits = network(opt, x_input)
-    
-    target_input = Input((1,), name='target_input')
-    center = Embedding(10, opt.embedding_size)(target_input)
-    l2_loss = Lambda(lambda x: K.sum(K.square(x[0] - x[1][:, 0]), 1, keepdims=True), name='l2_loss')([pre_logits, center])
+    shared_model = tf.keras.models.Model(inputs=[x_input], outputs=[softmax, pre_logits])
+    softmax, pre_logits = shared_model([x_input])
 
-    tf.keras.backend.clear_session()
-    tf.compat.v1.reset_default_graph()
-    model = tf.keras.models.Model(inputs=[x_input, target_input], outputs=[softmax, l2_loss])
+    target_input = Input((1,), name='target_input')
+    center = Dense(opt.embedding_size)(target_input)
+    shared_model = tf.keras.models.Model(inputs=[target_input], outputs=[center])
+    center = shared_model([target_input])
+
+    merged_pre = concatenate([pre_logits, center], axis=-1, name='merged_pre')
+
+    model = tf.keras.models.Model(inputs=[x_input, target_input], outputs=[softmax, merged_pre])
+
     model.compile(loss=["categorical_crossentropy", triplet_center_loss],
-                  optimizer=tf.keras.optimizers.Adam(), metrics=["accuracy"],
+                  optimizer=AngularGrad(), metrics=["accuracy"],
                   loss_weights=loss_weights)
 
     model.fit(x=[x_train, y_train], y=[y_train_onehot, y_train],
-              batch_size=opt.batch_size, epochs=opt.epoch, callbacks=[TensorBoard(log_dir=outdir)], validation_split=0.1)
+              batch_size=opt.batch_size, epochs=opt.epoch, validation_split=0.2)
 
     model.save(outdir + "triplet_center_loss_model.h5")
 
-    model = Model(inputs=[x_input, target_input], outputs=[softmax, l2_loss, pre_logits])
-    model.load_weights(outdir + "triplet_center_loss_model.h5")
+    # model = Model(inputs=[x_input], outputs=[softmax, pre_logits])
+    # model.load_weights(outdir + "center_loss_model.h5")
 
-    _, _, X_train_embed = model.predict([x_train, y_train])
-    y_test_soft, _, X_test_embed = model.predict([x_test, y_test])
+    _,           X_train_embed  = model.predict([x_train, y_train])
+    y_test_soft, X_test_embed   = model.predict([x_test, y_test])
 
-    return X_train_embed, X_test_embed, y_test_soft
+    return X_train_embed[:, :512], X_test_embed[:, :512], y_test_soft
+
