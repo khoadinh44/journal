8398d62b394d0c95aafc35d75454f726d3e06798
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-20 22:11:11 +0700

    Update triplet_center_loss.py

--- train_routines/triplet_center_loss.py
+++ train_routines/triplet_center_loss.py
@@ -53,13 +53,12 @@ def train_triplet_center_loss(opt, x_train, y_train, x_test, y_test, network):
     
 
     # model = Model(inputs=[x_input], outputs=[softmax, pre_logits])
-    # model.load_weights(outdir + "center_loss_model.h5")
     x_train, y_train = choosing_features(x_train, y_train)
     _,           X_train_embed  = model.predict([x_train, y_train])
     y_test_soft, X_test_embed   = model.predict([x_test, y_test])
     
     from TSNE_plot import tsne_plot
-    tsne_plot(outdir, "triplet_center_loss_model", X_train_embed[:, :512], X_test_embed[:, :512], y_train, y_test)
+    tsne_plot(outdir, "triplet_center_loss_model", X_train_embed[:, :256], X_test_embed[:, :256], y_train, y_test)
     
-    return X_train_embed[:, :512], X_test_embed[:, :512], y_test_soft, y_train
+    return X_train_embed[:, :256], X_test_embed[:, :256], y_test_soft, y_train
 
