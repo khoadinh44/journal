f0e1b130086d2624b5b8a2f1b781dc145a6dd917
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-20 21:07:42 +0700

    Update arc_model.py

--- src/arc_model.py
+++ src/arc_model.py
@@ -1,85 +1,48 @@
+import os
+from functools import partial
 import tensorflow as tf
-from tensorflow.keras import Model
-from tensorflow.keras.layers import (
-    Dense,
-    Dropout,
-    Flatten,
-    Input,
-)
-from .model import CNN_C_trip
-from .layers import (
-    BatchNormalization,
-    ArcMarginPenaltyLogists
-)
+import keras
+from tensorflow_addons.layers import MultiHeadAttention
+from tensorflow.keras.layers import Conv1D, Activation, Dense, concatenate
+import keras.backend as K
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras import layers
+from tensorflow.keras import regularizers
+from tensorflow.keras.layers import Activation, BatchNormalization, Conv1D, Dense, GlobalAveragePooling1D, Input, MaxPooling1D, Lambda
+from tensorflow.keras.models import Model
+from angular_grad import AngularGrad
+from src.arc_model import ArcFaceModel
+from src.losses import SoftmaxLoss
 
+def train_ArcFaceModel(opt, x_train, y_train, x_test, y_test, i=100):
+    print("\n Training with Triplet Loss....")
 
-def _regularizer(weights_decay=5e-4):
-    return tf.keras.regularizers.l2(weights_decay)
-
-
-def Backbone(opt, input_, backbone):
-    """Backbone Model"""
-    x = CNN_C_trip(opt, input_, backbone)
-    return x
-
-
-def OutputLayer(embd_shape, w_decay=5e-4, name='OutputLayer'):
-    """Output Later"""
-    def output_layer(x_in):
-        x = inputs = Input(x_in.shape[1:])
-        x = BatchNormalization()(x)
-        x = Dropout(rate=0.5)(x)
-        x = Flatten()(x)
-        x = Dense(embd_shape, kernel_regularizer=_regularizer(w_decay))(x)
-        x = BatchNormalization()(x)
-        return Model(inputs, x, name=name)(x_in)
-    return output_layer
-
-
-def ArcHead(num_classes, margin=0.5, logist_scale=64, name='ArcHead'):
-    """Arc Head"""
-    def arc_head(x_in, y_in):
-        x = inputs1 = Input(x_in.shape[1:])
-        y = Input(y_in.shape[1:])
-        x = ArcMarginPenaltyLogists(num_classes=num_classes,
-                                    margin=margin,
-                                    logist_scale=logist_scale)(x, y)
-        return Model((inputs1, y), x, name=name)((x_in, y_in))
-    return arc_head
-
-
-def NormHead(opt=None, w_decay=5e-4, name='NormHead'):
-    """Norm Head"""
-    def norm_head(x_in):
-        x = inputs = Input(x_in.shape[1:])
-        x = Dense(opt.num_classes, kernel_regularizer=_regularizer(w_decay))(x)
-        return Model(inputs, x, name=name)(x_in)
-    return norm_head
-
-
-def ArcFaceModel(opt=None, 
-                 channels=1, 
-                 name='arcface_model',
-                 margin=0.5, 
-                 logist_scale=64, 
-                 head_type='ArcHead', 
-                 training=False):
-    """Arc Face Model"""
-    x = inputs = Input([opt.input_shape, 1], name='input_signal')
-
-    x = Backbone(opt, x, True)(x)
-
-    embds = OutputLayer(opt.embedding_size, w_decay=5e-4)(x)
-
-    if training:
-        assert num_classes is not None
-        labels = Input([], name='label')
-        if head_type == 'ArcHead':
-            logist = ArcHead(num_classes=num_classes, 
-                             margin=margin,
-                             logist_scale=logist_scale)(embds, labels)
-        else:
-            logist = NormHead(opt=opt)(embds)
-        return Model((inputs, labels), logist, name=name)
+    outdir = opt.outdir + "/ArcFace/"
+    if i==0:
+      epoch = 50 # 30
     else:
-        return Model(inputs, embds, name=name)
+      epoch = opt.epoch # 10
+
+    if not os.path.isdir(outdir):
+        os.makedirs(outdir)
+        
+    loss_fn = SoftmaxLoss()
+    
+    model = ArcFaceModel(opt=opt)
+    model.summary(line_length=80)
+    model.compile(optimizer=AngularGrad(), loss=loss_fn)
+    
+    # Fit data-------------------------------------------------
+    model.fit(x=[x_train, y_train], y=y_train,
+              batch_size=opt.batch_size, epochs=epoch, callbacks=[TensorBoard(log_dir=outdir)], shuffle=True)
+    tf.saved_model.save(model, outdir + 'ArcFace')
+    
+    model = ArcFaceModel(opt=opt, training=False)
+    model.load_weights(outdir + 'ArcFace')
+    
+    X_train_embed = model.predict([x_train])
+    X_test_embed = model.predict([x_test])
+
+    from TSNE_plot import tsne_plot
+    tsne_plot(outdir, 'ArcFace', X_train_embed, X_test_embed, y_train, y_test)
+    return X_train_embed, X_test_embed, y_train
