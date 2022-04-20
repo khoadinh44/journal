5dbe8fda429e435f7c943c6bce698881c4aa70da
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-20 20:31:11 +0700

    Update arc_face.py

--- arc_face.py
+++ arc_face.py
@@ -9,4 +9,35 @@ from tensorflow.keras import layers
 from tensorflow.keras import regularizers
 from tensorflow.keras.layers import Activation, BatchNormalization, Conv1D, Dense, GlobalAveragePooling1D, Input, MaxPooling1D, Lambda
 from tensorflow.keras.models import Model
+from scr.arc_model import ArcFaceModel
+from scr.losses import SoftmaxLoss
 
+def train_ArcFaceModel(opt, x_train, y_train, x_test, y_test, network, i=100):
+    print("\n Training with Triplet Loss....")
+
+    outdir = opt.outdir + "/ArcFace/"
+    if i==0:
+      epoch = 50 # 30
+    else:
+      epoch = opt.epoch # 10
+
+    if not os.path.isdir(outdir):
+        os.makedirs(outdir)
+        
+    loss_fn = SoftmaxLoss()
+    
+    model = ArcFaceModel(opt=opt)
+    model.summary(line_length=80)
+    model.compile(optimizer=AngularGrad(), loss=loss_fn)
+    
+    # Fit data-------------------------------------------------
+    model.fit(x=[x_train, y_train], y=y_train,
+              batch_size=opt.batch_size, epochs=epoch, callbacks=[TensorBoard(log_dir=outdir)], shuffle=True)
+    tf.saved_model.save(model, outdir + 'ArcFace')
+    
+    model = ArcFaceModel(opt=opt, training=False)
+    model.load_weights(outdir + 'ArcFace')
+    
+    X_train_embed = model.predict([x_train])
+    X_test_embed = model.predict([x_test])
+    return X_train_embed, X_test_embed, y_train
