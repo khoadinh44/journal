d8f2e9cd16cb639091013023f17493a55c6a1183
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-20 13:26:39 +0700

    Update losses.py

--- src/losses.py
+++ src/losses.py
@@ -7,7 +7,6 @@ def SoftmaxLoss():
         # y_true: sparse target
         # y_pred: logist
         y_true = tf.cast(tf.reshape(y_true, [-1]), tf.int32)
-        ce = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_true,
-                                                            logits=y_pred)
+        ce = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred)
         return tf.reduce_mean(ce)
     return softmax_loss
