2388867bebcbc6efb329e5a23649cf9f9f53fdab
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-21 09:09:55 +0700

    Update train_embedding_model.py

--- train_embedding_model.py
+++ train_embedding_model.py
@@ -10,7 +10,7 @@ from train_routines.triplet_loss import train
 from train_routines.center_loss import train_center_loss
 from train_routines.triplet_center_loss import train_triplet_center_loss
 from train_routines.new_triplet_center import train_new_triplet_center
-from preprocessing.utils import handcrafted_features
+from preprocessing.utils import handcrafted_features, FFT
 
 from sklearn.utils import shuffle
 from scipy.spatial.distance import cosine, euclidean
@@ -42,42 +42,46 @@ def main(opt):
 
   if os.path.exists('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/y_train.npy'):
     X_train = np.load('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/X_train.npy')
-    X_test = np.load('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/X_test.npy')
+    X_test  = np.load('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/X_test.npy')
     y_train = np.load('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/y_train.npy')
-    y_test = np.load('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/y_test.npy')
+    y_test  = np.load('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/y_test.npy')
   else:
-    X_train, X_test, y_train, y_test = get_data(opt)
+     X_train, X_test, y_train, y_test = get_data(opt)
 
-    print('\n Converting data...')
-    y_train = invert_one_hot(y_train)
-    y_test = invert_one_hot(y_test)
+     print('\n Converting data...')
+     y_train = invert_one_hot(y_train)
+     y_test = invert_one_hot(y_test)
 
 
-    with open('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/y_train.npy', 'wb') as f:
-      np.save(f, y_train)
-    with open('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/y_test.npy', 'wb') as f:
-      np.save(f, y_test)
-    with open('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/X_train.npy', 'wb') as f:
-      np.save(f, X_train)
-    with open('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/y_train.npy', 'wb') as f:
-      np.save(f, y_train)
-    with open('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/X_test.npy', 'wb') as f:
-      np.save(f, X_test)
+     with open('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/y_train.npy', 'wb') as f:
+       np.save(f, y_train)
+     with open('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/y_test.npy', 'wb') as f:
+       np.save(f, y_test)
+     with open('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/X_train.npy', 'wb') as f:
+       np.save(f, X_train)
+     with open('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/y_train.npy', 'wb') as f:
+       np.save(f, y_train)
+     with open('/content/drive/Shareddrives/newpro112233/signal_machine/output_triplet_loss/X_test.npy', 'wb') as f:
+       np.save(f, X_test)
 
   print(f' Training data shape: {X_train.shape},  Training label shape: {y_train.shape}')
   print(f' Testing data shape: {X_test.shape},    Testing label shape: {y_test.shape}')
+
+  if opt.use_FFT:
+    X_train_FFT = FFT(X_train)
+    X_test_FFT = FFT(X_test)
   
   print('\n Loading model...')
   if opt.embedding_model == 'triplet':
-    train_embs, test_embs, y_test_solf, y_train = train(opt, X_train, y_train, X_test, y_test, CNN_C_trip) 
+    train_embs, test_embs, y_test_solf, y_train = train(opt, X_train_FFT, y_train, X_test_FFT, y_test, CNN_C_trip) 
   if opt.embedding_model == 'center':
-    train_embs, test_embs, y_test_solf, y_train = train_center_loss(opt, X_train, y_train, X_test, y_test, CNN_C_trip) 
+    train_embs, test_embs, y_test_solf, y_train = train_center_loss(opt, X_train_FFT, y_train, X_test_FFT, y_test, CNN_C_trip) 
   if opt.embedding_model == 'triplet_center':
-    train_embs, test_embs, y_test_solf, y_train = train_triplet_center_loss(opt, X_train, y_train, X_test, y_test, CNN_C_trip) 
+    train_embs, test_embs, y_test_solf, y_train = train_triplet_center_loss(opt, X_train_FFT, y_train, X_test_FFT, y_test, CNN_C_trip) 
   if opt.embedding_model == 'new_triplet_center':
-    train_embs, test_embs, y_test_solf, y_train = train_new_triplet_center(opt, X_train, y_train, X_test, y_test, CNN_C_trip)
+    train_embs, test_embs, y_test_solf, y_train = train_new_triplet_center(opt, X_train_FFT, y_train, X_test_FFT, y_test, CNN_C_trip)
   if opt.embedding_model == 'arcface':
-    train_embs, test_embs, y_train = train_ArcFaceModel(opt, X_train, y_train, X_test, y_test)
+    train_embs, test_embs, y_train = train_ArcFaceModel(opt, X_train_FFT, y_train, X_test_FFT, y_test)
     
 
   print('\n Saving embedding phase...')   
@@ -100,6 +104,8 @@ def main(opt):
   for each_ML in ['SVM', 'RandomForestClassifier', 'LogisticRegression', 'GaussianNB', 'KNN', 'BT', 'euclidean', 'cosine']:
     model = FaceNetOneShotRecognitor(opt, X_train, y_train, X_test, y_test) 
     y_pred = model.predict(test_embs=test_embs, train_embs=train_embs, ML_method=each_ML)
+    count1 += 1
+    acc = accuracy_score(y_test, y_pred)
 
     y_pred_onehot = to_one_hot(y_pred)
     if y_pred_all == []:
@@ -120,23 +126,20 @@ def main(opt):
       else:
         y_pred_BT_RandomForestClassifier_cosine += y_pred_onehot
       count3 += 1
-      
-
-    count1 += 1
-    acc = accuracy_score(y_test, y_pred)
 
     print(f'\n-------------- 1.Test accuracy: {acc} with the {each_ML} method--------------')
-    # X_train_hand = handcrafted_features(X_train)
-    # X_test_hand  = handcrafted_features(X_test)
-    # model = FaceNetOneShotRecognitor(opt, X_train_hand, y_train, X_test_hand, y_test) 
-    # y_pred_no_emb = model.predict(test_embs=test_embs, train_embs=train_embs, ML_method=each_ML, emb=False)
-    # y_pred_onehot_no_emb = to_one_hot(y_pred_no_emb)
+   
+    X_train_hand = handcrafted_features(X_train)
+    X_test_hand  = handcrafted_features(X_test)
+    model = FaceNetOneShotRecognitor(opt, X_train_hand, y_train, X_test_hand, y_test) 
+    y_pred_no_emb = model.predict(test_embs=test_embs, train_embs=train_embs, ML_method=each_ML, emb=False)
+    y_pred_onehot_no_emb = to_one_hot(y_pred_no_emb)
     
-#     y_pred_all += y_pred_onehot_no_emb
-#     count += 1
-    # acc = accuracy_score(y_test, y_pred_no_emb)
+    y_pred_all += y_pred_onehot_no_emb
+    count1 += 1
+    acc = accuracy_score(y_test, y_pred_no_emb)
 
-    # print(f'\n-------------- 2.Test accuracy: {acc} with the {each_ML} method--------------')
+    print(f'\n-------------- 2.Test accuracy: {acc} with the {each_ML} method--------------')
   
   y_pred_SVM_RandomForestClassifier = y_pred_SVM_RandomForestClassifier.astype(np.float32) / count2
   y_pred_SVM_RandomForestClassifier = np.argmax(y_pred_SVM_RandomForestClassifier, axis=1)
