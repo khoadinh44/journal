5f7f6822651fd87aa373df789a61eaa23019468e
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-04-28 15:29:59 +0700

    Update train_embedding_model.py

--- train_embedding_model.py
+++ train_embedding_model.py
@@ -163,7 +163,7 @@ def main(opt):
   if opt.embedding_model == 'triplet_center':
     train_embs, test_embs, y_test_solf, y_train, outdir = train_triplet_center_loss(opt, X_train_FFT, y_train, X_test_FFT, y_test, CNN_C_trip) 
   if opt.embedding_model == 'new_triplet_center':
-    train_embs, test_embs, y_test_solf, y_train, outdir = train_new_triplet_center(opt, X_train_FFT, y_train, X_test_FFT, y_test, CNN_C_trip)
+    train_embs, test_embs, y_test_solf, y_train, outdir = train_new_triplet_center(opt, X_train_FFT, X_train, y_train, X_test_FFT, X_test, y_test, CNN_C_trip)
   if opt.embedding_model == 'arcface':
     train_embs, test_embs, y_train, outdir = train_ArcFaceModel(opt, X_train_FFT, y_train, X_test_FFT, y_test)
   
