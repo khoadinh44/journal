79d4d0d7824a8a61a6d81968e6b0eb5a9afaf779
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-04 15:16:28 +0700

    Update triplet.py

--- triplet.py
+++ triplet.py
@@ -210,38 +210,38 @@ def product(x, y):
 
 #     return 0.5*loss + loss_extract
 
-# def triplet_loss(y_true, y_pred, alpha=0.4, lambda_=opt.lambda_):
-#     """
-#     Implementation of the triplet loss function
-#     Arguments:
-#     y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
-#     y_pred -- python list containing three objects:
-#             anchor -- the encodings for the anchor data
-#             positive -- the encodings for the positive data (similar to anchor)
-#             negative -- the encodings for the negative data (different from anchor)
-#     Returns:
-#     loss -- real number, value of the loss
-#     """
-#     total_lenght = y_pred.shape.as_list()[-1]
+def triplet_loss(y_true, y_pred, alpha=0.4, lambda_=opt.lambda_):
+    """
+    Implementation of the triplet loss function
+    Arguments:
+    y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
+    y_pred -- python list containing three objects:
+            anchor -- the encodings for the anchor data
+            positive -- the encodings for the positive data (similar to anchor)
+            negative -- the encodings for the negative data (different from anchor)
+    Returns:
+    loss -- real number, value of the loss
+    """
+    total_lenght = y_pred.shape.as_list()[-1]
 
-#     anchor   = y_pred[:, 0:int(total_lenght * 1 / 3)]
-#     anchor   = tf.math.l2_normalize(anchor, axis=1, epsilon=1e-10)
-#     positive = y_pred[:, int(total_lenght * 1 / 3):int(total_lenght * 2 / 3)]
-#     positive = tf.math.l2_normalize(positive, axis=1, epsilon=1e-10)
-#     negative = y_pred[:, int(total_lenght * 2 / 3):int(total_lenght * 3 / 3)]
-#     negative = tf.math.l2_normalize(negative, axis=1, epsilon=1e-10)
+    anchor   = y_pred[:, 0:int(total_lenght * 1 / 3)]
+    anchor   = tf.math.l2_normalize(anchor, axis=1, epsilon=1e-10)
+    positive = y_pred[:, int(total_lenght * 1 / 3):int(total_lenght * 2 / 3)]
+    positive = tf.math.l2_normalize(positive, axis=1, epsilon=1e-10)
+    negative = y_pred[:, int(total_lenght * 2 / 3):int(total_lenght * 3 / 3)]
+    negative = tf.math.l2_normalize(negative, axis=1, epsilon=1e-10)
 
-#     # distance between the anchor and the positive
-#     pos_dist          = K.sum(K.square(anchor - positive), axis=1)
+    # distance between the anchor and the positive
+    pos_dist          = K.sum(K.square(anchor - positive), axis=1)
 
-#     # distance between the anchor and the negative
-#     neg_dist          = K.sum(K.square(anchor - negative), axis=1)
+    # distance between the anchor and the negative
+    neg_dist          = K.sum(K.square(anchor - negative), axis=1)
 
-#     # compute loss
-#     basic_loss = pos_dist - neg_dist + alpha
-#     loss       = K.maximum(basic_loss, 0.0)
+    # compute loss
+    basic_loss = pos_dist - neg_dist + alpha
+    loss       = K.maximum(basic_loss, 0.0)
 
-#     return loss
+    return loss
 
 
 def triplet_center_loss(y_true, y_pred, n_classes= 3, alpha=0.4):
