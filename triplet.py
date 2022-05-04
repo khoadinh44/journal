4ba7cdc66b054099c9bd5f86ea7cff328faf2d6d
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-04 16:48:46 +0700

    Update triplet.py

--- triplet.py
+++ triplet.py
@@ -8,7 +8,7 @@ from train import parse_opt
 opt = parse_opt()
 
 
-def generate_triplet(x, y,  ap_pairs=12, an_pairs=12):
+def generate_triplet(x, y,  ap_pairs=16, an_pairs=16):
     data_xy = tuple([x, y])
     trainsize = 1
 
@@ -18,9 +18,9 @@ def generate_triplet(x, y,  ap_pairs=12, an_pairs=12):
     for data_class in sorted(set(data_xy[1])):
         same_class_idx = np.where((data_xy[1] == data_class))[0]
         diff_class_idx = np.where(data_xy[1] != data_class)[0]
-        pair = min(len(list(permutations(same_class_idx, 2))), len(diff_class_idx))
-        A_P_pairs = random.sample(list(permutations(same_class_idx, 2)), k=pair)  # Generating Anchor-Positive pairs
-        Neg_idx = random.sample(list(diff_class_idx), k=pair)
+        # pair = min(len(list(permutations(same_class_idx, 2))), len(diff_class_idx))
+        A_P_pairs = random.sample(list(permutations(same_class_idx, 2)), k=ap_pairs)  # Generating Anchor-Positive pairs
+        Neg_idx = random.sample(list(diff_class_idx), k=an_pairs)
 
         # train
         A_P_len = len(A_P_pairs)
