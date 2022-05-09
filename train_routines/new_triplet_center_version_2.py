60b1fca63287f43adbd2dd003dddc3aa7abdfae3
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-09 17:46:50 +0700

    Update new_triplet_center_version_2.py

--- train_routines/new_triplet_center_version_2.py
+++ train_routines/new_triplet_center_version_2.py
@@ -25,15 +25,20 @@ from angular_grad import AngularGrad
 callback = tf.keras.callbacks.EarlyStopping(monitor='loss', mode='min', verbose=1, patience=2)
 
 def extracted_model(in_, opt):
-  x = Dense(opt.embedding_size*4,
+  fc1 = Dense(opt.embedding_size*4,
             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
             bias_regularizer=regularizers.l2(1e-4),
             activity_regularizer=regularizers.l2(1e-5))(in_)
-  x = Dense(opt.embedding_size*2,
+  fc2 = Dense(opt.embedding_size*4,
+            kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+            bias_regularizer=regularizers.l2(1e-4),
+            activity_regularizer=regularizers.l2(1e-5))(in_)
+  x = concatenate([fc1, fc2], axis=-1)
+  x = Dense(opt.embedding_size*4,
             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
             bias_regularizer=regularizers.l2(1e-4),
             activity_regularizer=regularizers.l2(1e-5))(x)
-  x = concatenate([x, in_], axis=-1)
+  x = BatchNormalization()(x)
   x = Dropout(rate=0.5)(x)
   x = Dense(opt.embedding_size)(x)
   x = BatchNormalization()(x)
