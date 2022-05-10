111d33119e9903f7f6abf88953831497663b65eb
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-10 16:40:17 +0700

    Update new_triplet_center_version_2.py

--- train_routines/new_triplet_center_version_2.py
+++ train_routines/new_triplet_center_version_2.py
@@ -33,11 +33,6 @@ def extracted_model(in_, opt):
             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
             bias_regularizer=regularizers.l2(1e-4),
             activity_regularizer=regularizers.l2(1e-5))(x)
-  x = Dropout(rate=0.1)(x)
-  x = Dense(opt.embedding_size*4,
-            kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-            bias_regularizer=regularizers.l2(1e-4),
-            activity_regularizer=regularizers.l2(1e-5))(x)
   x = Dropout(rate=0.5)(x)
   x = concatenate([x, in_], axis=-1)
   x = Dense(opt.embedding_size)(x)
