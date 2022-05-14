8b0601220e0ca46b4a76936d40c2ffd448b97108
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-14 14:39:24 +0700

    Update new_triplet_center_version_2.py

--- train_routines/new_triplet_center_version_2.py
+++ train_routines/new_triplet_center_version_2.py
@@ -25,19 +25,35 @@ from angular_grad import AngularGrad
 callback = tf.keras.callbacks.EarlyStopping(monitor='loss', mode='min', verbose=1, patience=2)
 
 def extracted_model(in_, opt):
-  x = Dense(opt.embedding_size*2,
+  x = Dense(opt.embedding_size*4,
             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
             bias_regularizer=regularizers.l2(1e-4),
             activity_regularizer=regularizers.l2(1e-5))(in_)
+  # x = Dropout(rate=0.1)(x)
+  x = Dense(opt.embedding_size*2,
+            kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+            bias_regularizer=regularizers.l2(1e-4),
+            activity_regularizer=regularizers.l2(1e-5))(x)
+  # x = Dropout(rate=0.1)(x)
+  x = Dense(opt.embedding_size,
+            kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+            bias_regularizer=regularizers.l2(1e-4),
+            activity_regularizer=regularizers.l2(1e-5))(x)
+  # x = Dropout(rate=0.1)(x)
+  x = Dense(opt.embedding_size*2,
+            kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+            bias_regularizer=regularizers.l2(1e-4),
+            activity_regularizer=regularizers.l2(1e-5))(x)
+  # x = Dropout(rate=0.1)(x)
   x = Dense(opt.embedding_size*4,
             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
             bias_regularizer=regularizers.l2(1e-4),
             activity_regularizer=regularizers.l2(1e-5))(x)
   x = Dropout(rate=0.5)(x)
-  x = concatenate([x, in_], axis=-1)
+  # x = concatenate([x, in_], axis=-1)
   x = Dense(opt.embedding_size)(x)
   x = BatchNormalization()(x)
-  # x = Lambda(lambda  x: K.l2_normalize(x, axis=1))(x)
+  x = Activation('relu')(x)
   return x
 
 def train_new_triplet_center(opt, x_train_scale, x_train, y_train, x_test_scale, x_test, y_test, network, i=100):
