e00c5334bd5defc6ef3218a47f481b978d5a0521
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-15 22:09:41 +0700

    Update new_triplet_center_version_2.py

--- train_routines/new_triplet_center_version_2.py
+++ train_routines/new_triplet_center_version_2.py
@@ -79,7 +79,7 @@ def train_new_triplet_center(opt, x_train_scale, x_train, y_train, x_test_scale,
 
     outdir = opt.outdir + "/new_triplet_loss/"
     if i==0:
-      epoch = 50 # 30
+      epoch = 100 # 30
     else:
       epoch = opt.epoch # 10
 
